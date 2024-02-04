9d72fa9396dd81787a0d4f2698aeba21d4131038
mrm15 <mrm.learn@gmail.com>
2024-02-04 22:14:45 +0330

    Update verifyJWT.ts

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -2,11 +2,11 @@ import { Request as ExpressRequest, Response, NextFunction } from 'express';
 import jwt, { JwtPayload, VerifyErrors } from 'jsonwebtoken';
 
 // Define a custom Request type that extends ExpressRequest
-interface CustomRequest extends ExpressRequest {
-    myToken?: JwtPayload
+export interface CustomRequestMyTokenInJwt extends ExpressRequest {
+    myToken?: JwtPayload['myToken']
 }
 
-const verifyJWT = (req: CustomRequest, res: Response, next: NextFunction): void => {
+const verifyJWT = (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction): void => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
 
     const authHeaderValue = Array.isArray(authHeader) ? authHeader[0] : authHeader; // Ensure authHeader is a string
@@ -30,7 +30,7 @@ const verifyJWT = (req: CustomRequest, res: Response, next: NextFunction): void 
             } //invalid token
             // Assign userInfo directly to req object
             debugger
-            req.body.myToken = decoded
+            req.myToken = decoded
             next();
         }
     );
