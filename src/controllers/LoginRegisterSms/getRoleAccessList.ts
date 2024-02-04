0710817956d42e8a6fbcbb4f1cffa4fddbe97843
mrm15 <mrm.learn@gmail.com>
2024-02-04 10:48:02 +0330

    Update getRoleAccessList.ts

--- src/controllers/LoginRegisterSms/getRoleAccessList.ts
+++ src/controllers/LoginRegisterSms/getRoleAccessList.ts
@@ -1,18 +1,33 @@
-import {Types} from "mongoose";
-import {Role, IRole} from "../../models/roles";
-import {IUser, User} from "../../models/User";
+import { Types } from "mongoose";
+import { Role, IRole } from "../../models/roles";
+import { IUser, User } from "../../models/User";
 
-export const getRoleAccessList = async (phoneNumber: string): Promise<Partial<IRole> | null> => {
+// Define the type for the keys of roleAccessList
 
-    const foundUser = await User.findOne({phoneNumber}).exec();
+
+export const getRoleAccessList = async (phoneNumber: string): Promise<string[] | null> => {
+    const foundUser = await User.findOne({ phoneNumber }).exec();
     const userRoleId = foundUser?.role;
 
     if (userRoleId) {
-        const roleObject = await Role.findOne({_id: userRoleId}).exec();
+        const roleObject = await Role.findOne({ _id: userRoleId }).exec();
         if (roleObject) {
             // Destructure the properties you want to exclude and spread the rest into a new object
-            const {_id,__v,name, description, createAt, updateAt, userId, ...roleAccessList} = roleObject.toObject();
-            return roleAccessList;
+            const { _id, __v, name, description, createAt, updateAt, userId, ...roleAccessList } = roleObject.toObject();
+
+            const roleAccessListArray: string[] = [];
+
+
+
+            const temp:any = {...roleAccessList}
+            for (const key in temp) {
+                // Use type assertion here
+                if (temp[key]===true) {
+                    roleAccessListArray.push(key);
+                }
+            }
+
+            return roleAccessListArray;
         }
     }
 
