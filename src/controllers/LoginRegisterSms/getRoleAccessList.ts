856d7b0d7fe7fc802d813ecb8646cee6e14d3788
mrm15 <mrm.learn@gmail.com>
2024-02-05 11:36:41 +0330

    Update getRoleAccessList.ts

--- src/controllers/LoginRegisterSms/getRoleAccessList.ts
+++ src/controllers/LoginRegisterSms/getRoleAccessList.ts
@@ -7,18 +7,19 @@ import { IUser, User } from "../../models/User";
 
 export const getRoleAccessList = async (phoneNumber: string): Promise<string[] | null> => {
     const foundUser = await User.findOne({ phoneNumber }).exec();
+    // console.log("foundUser")
+    // console.log(foundUser)
     const userRoleId = foundUser?.role;
+    // console.log("userRoleId")
+    // console.log(userRoleId)
+
 
     if (userRoleId) {
         const roleObject = await Role.findOne({ _id: userRoleId }).exec();
         if (roleObject) {
             // Destructure the properties you want to exclude and spread the rest into a new object
             const { _id, __v, name, description, createAt, updateAt, userId, ...roleAccessList } = roleObject.toObject();
-
             const roleAccessListArray: string[] = [];
-
-
-
             const temp:any = {...roleAccessList}
             for (const key in temp) {
                 // Use type assertion here
