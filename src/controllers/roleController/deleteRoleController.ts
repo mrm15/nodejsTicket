587cd63e68c873ae6ece8964b7018634ea07da22
mrm15 <mrm.learn@gmail.com>
2024-02-12 10:16:55 +0330

    Update deleteRoleController.ts

--- src/controllers/roleController/deleteRoleController.ts
+++ src/controllers/roleController/deleteRoleController.ts
@@ -8,6 +8,7 @@ import {uuidGenerator} from "../../utils/uuidGenerator";
 import {getUserInfoByPhoneNumber} from "../LoginRegisterSms/getUserInfoByPhoneNumber";
 import {ACCESS_LIST} from "../../utils/ACCESS_LIST";
 import {checkAccessList} from "../../utils/checkAccessList";
+import {IRole, Role} from "../../models/roles";
 
 
 const deleteRoleController = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
@@ -22,34 +23,34 @@ const deleteRoleController = async (req: CustomRequestMyTokenInJwt, res: Respons
 
         const {id} = req.params;
         try {
-            const requestUser: IUser =(await User.findOne({_id:id}).exec())!;
-            if (requestUser.phoneNumber === myToken.phoneNumber) {
+            const foundUser: IUser | null = await User.findOne({role: id}).exec()
+            if (foundUser) {
                 res.status(409).json({
-                    message: `عزیزم تو نمیتونی خودت رو حذف کنی.🙄`
+                    message: `برای حذف نقش ابتدا کاربرانی که این نقش را دارند حذف کنید!!!.🙄`
                 });
                 return
             }
 
-        } catch (error:any) {
+        } catch (error: any) {
             res.status(500).json({message: 'Error deleting user', error: error?.message});
             return
         }
 
         // Attempt to find and delete the user by ID
-        const deletedUser = await User.findByIdAndDelete(id);
+        const deletedRole = await Role.findByIdAndDelete(id);
 
         // Check if a user was found and deleted
-        if (!deletedUser) {
-            res.status(404).json({message: 'User not found'});
+        if (!deletedRole) {
+            res.status(404).json({message: 'ROLE not found'});
             return
         }
 
         // Successfully deleted the user
-        res.status(200).json({message: `کاربر با شماره ${deletedUser.phoneNumber} برای همیشه حذف شد.`,});
+        res.status(200).json({message: `نقش با نام ${deletedRole.name} برای همیشه حذف شد.`,});
         return
     } catch (error: any) {
         // Handle potential errors, such as invalid ObjectId format
-        res.status(500).json({message: 'Error deleting user', error: error?.message});
+        res.status(500).json({message: 'Error deleting ROLE', error: error?.message});
         return
     }
 
