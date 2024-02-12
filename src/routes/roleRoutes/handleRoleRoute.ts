3bde0d1d30cf073d6c65c9ba1a1b49d9ead59fa0
mrm15 <mrm.learn@gmail.com>
2024-02-12 10:20:04 +0330

    Update handleRoleRoute.ts

--- src/routes/roleRoutes/handleRoleRoute.ts
+++ src/routes/roleRoutes/handleRoleRoute.ts
@@ -3,6 +3,7 @@ import {createRoleController} from '../../controllers/roleController/createRoleC
 import {readRoleController} from "../../controllers/roleController/readRoleController";
 import {updateRoleController} from "../../controllers/roleController/updateRoleController";
 import {deleteRoleController} from "../../controllers/roleController/deleteRoleController";
+import {roleList} from "../../controllers/roleController/roleList";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.post('/create', createRoleController);
 router.get('/read', readRoleController);
 router.post('/update', updateRoleController);
 router.delete('/delete/:id', deleteRoleController);
+router.get('/roleList', roleList);
 
 export default router;
