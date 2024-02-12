16ff160811abb66b70b19e04bdc1173eb3ad173b
mrm15 <mrm.learn@gmail.com>
2024-02-12 10:19:43 +0330

    Update updateRoleController.ts

--- src/controllers/roleController/updateRoleController.ts
+++ src/controllers/roleController/updateRoleController.ts
@@ -1,46 +1,100 @@
 import {Request, Response, NextFunction} from 'express';
 import {IUser, User} from "../../models/User";
-import {File, IFile} from "../../models/files";
 import {getCurrentTimeStamp} from "../../utils/timing";
 import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
-import {addNewUserF} from "../LoginRegisterSms/addNewUserF";
-import {uuidGenerator} from "../../utils/uuidGenerator";
-import {getUserInfoByPhoneNumber} from "../LoginRegisterSms/getUserInfoByPhoneNumber";
 import {ACCESS_LIST} from "../../utils/ACCESS_LIST";
 import {checkAccessList} from "../../utils/checkAccessList";
+import {IRole, Role} from "../../models/roles";
+import {myPermissionsArray} from "./permissinsArray";
 
 
 const updateRoleController = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
 
+
         const {myToken} = req;
+        const newRoleData = req.body;
+
+        // res.status(201).json({myToken})
+        //
+        // return
+
         if (!myToken) {
             const message = 'مقدار توکن توی ری کوئست موجود نیست'
             res.status(403).json({message});
             return
         }
 
-
         try {
+
+
+            const {phoneNumber} = myToken
+
             const {id} = req.body
-            const foundUser: IUser = (await User.findOne({_id: id}).exec())!;
-            const newData = {...req.body}
-            delete newData.id;
-            delete newData._id;
-            for (const key  in newData) {
-                foundUser[key] = newData[key]
+
+
+            const arrayListToCheck = [
+                ACCESS_LIST.ROLES_UPDATE
+            ]
+            const hasAccessToUpdateRole = await checkAccessList({phoneNumber, arrayListToCheck})
+
+            if (!hasAccessToUpdateRole) {
+                res.status(403).json({message: 'شما مجوز دسترسی به این بخش را ندارید.'});
+                return
+            }
+
+            // check if this phone number is uniq
+            const foundRole: IRole = (await Role.findOne({_id: id}).exec())!;
+
+
+            if (!foundRole) {
+                res.status(500).json({message: "نقشی با این نام یافت نشد."});
+                return
             }
 
 
-            const result = await foundUser.save()
-            res.status(200).json({
-                message:"کاربر با موفقیت به روز شد."
-            });
+            const permissionsArray = [...myPermissionsArray]
+
+
+            const userFound: IUser | null = await User.findOne({phoneNumber: myToken.phoneNumber})
+
+            if (!userFound) {
+                res.status(500).json({message: "کاربری تایید نشد."});
+                return
+            }
+
+            const userId = userFound?.id
+
+
+            foundRole.name = newRoleData?.name || "بدون نام"
+            foundRole.description = newRoleData?.description || " بدون توضیحات"
+            foundRole.updateAt = getCurrentTimeStamp()
+            foundRole.userId = userId
+
+
+            permissionsArray.forEach(item => {
+                let result = false
+                newRoleData.statusListCreate.forEach((frontItem: string) => {
+                    if (item === frontItem) {
+                        result = true
+                    }
+                })
+                foundRole[item] = result
+            })
+
+            await foundRole.save()
+            res.status(200).json({message: 'نقش با موفقیت آپدیت شد',});
             return;
+
         } catch (error) {
             // console.log(error)
-            res.status(500).json({error});
+            res.status(500).json({
+                error: error?.toString(),
+
+            });
             return
         }
+
+
     }
 ;
 
