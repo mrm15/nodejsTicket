798f8defb374e017ef10b9483e495bea9e3bd92d
mrm15 <mrm.learn@gmail.com>
2024-02-18 11:54:01 +0330

    add  path to upload (read File)

--- src/routes/handleUpload/index.ts
+++ src/routes/handleUpload/index.ts
@@ -1,24 +1,15 @@
 import express, {Request, Response, NextFunction, Request as ExpressRequest, Router} from 'express';
 import {handleUpload} from '../../controllers/handleUplaodController/uplaod';
+import {readFilesController} from "../../controllers/handleUplaodController/readFilesController";
 
-import multer from 'multer';
-import path from "path";
-import {JwtPayload} from "jsonwebtoken";
-import {createDepartmentController} from "../../controllers/departmentController/createDepartmentController";
-import {readDepartmentController} from "../../controllers/departmentController/readDepartmentController";
-import {updateDepartmentController} from "../../controllers/departmentController/updateDepartmentController";
-import {deleteDepartmentController} from "../../controllers/departmentController/deleteDepartmentController";
-import {departmentList} from "../../controllers/departmentController/departmentList";
-import {readFileController} from "../../controllers/handleUplaodController/readFileController";
-import {updateFileController} from "../../controllers/handleUplaodController/updateFileController";
-import {deleteFileController} from "../../controllers/handleUplaodController/deleteFileController";
-import {getFileController} from "../../controllers/handleUplaodController/getFileController";
+
 
 const router = express.Router();
 
 
 
 router.post('/', handleUpload);
+router.get('/read', readFilesController);
 
 
 
