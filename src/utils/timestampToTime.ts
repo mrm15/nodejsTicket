f66aa894a97ee3ad7c79777bca6124d0252b317c
mrm15 <mrm.learn@gmail.com>
2024-02-18 11:54:36 +0330

    update function timestampToTime

--- src/utils/timestampToTime.ts
+++ src/utils/timestampToTime.ts
@@ -2,7 +2,10 @@ export const timestampToTime = (TimeStampDate: number | Date | undefined)=>{
     const formatter = new Intl.DateTimeFormat('fa-IR', {
         year: 'numeric',
         month: '2-digit',
-        day: '2-digit'
+        day: '2-digit',
+        hour:'2-digit',
+        minute:'2-digit',
+        second:'2-digit',
         // The 'calendar' option is not included here as it's not standard in all environments
     });
 
