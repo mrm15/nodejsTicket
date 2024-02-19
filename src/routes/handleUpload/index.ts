ba791837e446294d4a8958f8787038ff2d5d109c
mrm15 <mrm.learn@gmail.com>
2024-02-19 13:26:09 +0330

    Update index.ts

--- src/routes/handleUpload/index.ts
+++ src/routes/handleUpload/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response, NextFunction, Request as ExpressRequest, Router} from 'express';
 import {handleUpload} from '../../controllers/handleUplaodController/uplaod';
 import {readFilesController} from "../../controllers/handleUplaodController/readFilesController";
+import {deleteFilesController} from "../../controllers/handleUplaodController/deleteFilesController";
 
 
 
@@ -8,8 +9,9 @@ const router = express.Router();
 
 
 
-router.post('/', handleUpload);
+router.post('/', handleUpload); // upload a File to server  register in files DB and upload in uploads Folder
 router.get('/read', readFilesController);
+router.delete('/delete/:id', deleteFilesController);
 
 
 
