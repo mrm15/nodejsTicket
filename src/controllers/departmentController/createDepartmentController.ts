8fabf64b004ef17fdeb85d7403f89ae4f8e1043c
mrm15 <mrm.learn@gmail.com>
2024-02-24 23:37:46 +0330

    Update createDepartmentController.ts

--- src/controllers/departmentController/createDepartmentController.ts
+++ src/controllers/departmentController/createDepartmentController.ts
@@ -97,7 +97,7 @@ const createDepartmentController = async (req: CustomRequestMyTokenInJwt, res: R
         return;
 
     } catch (error) {
-        // console.log(error)
+
         res.status(500).json({
             error: error?.toString(),
         });
