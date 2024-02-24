6284f82838e1048d91de0c60422aeaf06a323b71
mrm15 <mrm.learn@gmail.com>
2024-02-24 23:37:50 +0330

    Update departmentList.ts

--- src/controllers/departmentController/departmentList.ts
+++ src/controllers/departmentController/departmentList.ts
@@ -45,7 +45,7 @@ const departmentList = async (req: CustomRequestMyTokenInJwt, res: Response, nex
         return;
 
     } catch (error) {
-        // console.log(error)
+
         res.status(500).json({error});
         return
     }
