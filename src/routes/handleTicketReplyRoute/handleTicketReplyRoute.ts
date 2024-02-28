83fef0d308189077620496867f3d0fea6368e180
mrm15 <mrm.learn@gmail.com>
2024-02-28 10:14:33 +0330

    add ticket Reply Controller Files

---
+++ src/routes/handleTicketReplyRoute/handleTicketReplyRoute.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import {createTicketReplyController} from "../../controllers/ticketReplyController/createTicketReplyController";
+import {readTicketReplyController} from "../../controllers/ticketReplyController/readTicketReplyController";
+import {updateTicketReplyController} from "../../controllers/ticketReplyController/updateTicketReplyController";
+import {deleteTicketReplyController} from "../../controllers/ticketReplyController/deleteTicketReplyController";
+import {ticketReplyList} from "../../controllers/ticketReplyController/ticketReplyList";
+
+
+const router = express.Router();
+
+router.post('/create', createTicketReplyController)
+router.get('/read', readTicketReplyController)
+router.post('/update', updateTicketReplyController);
+router.delete('/delete/:id', deleteTicketReplyController);
+router.get('/replyList', ticketReplyList);
+
+export default router;
