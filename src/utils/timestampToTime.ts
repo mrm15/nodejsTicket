f82a9a836e241ede80ce5e6954501076a2dca095
mrm15 <mrm.learn@gmail.com>
2024-02-28 14:00:28 +0330

    catch error in timestampToTime.ts

--- src/utils/timestampToTime.ts
+++ src/utils/timestampToTime.ts
@@ -1,13 +1,17 @@
-export const timestampToTime = (TimeStampDate: number | Date | undefined)=>{
-    const formatter = new Intl.DateTimeFormat('fa-IR', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour:'2-digit',
-        minute:'2-digit',
-        second:'2-digit',
-        // The 'calendar' option is not included here as it's not standard in all environments
-    });
+export const timestampToTime = (TimeStampDate: number | Date | undefined) => {
+    try {
+        const formatter = new Intl.DateTimeFormat('fa-IR', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            // The 'calendar' option is not included here as it's not standard in all environments
+        });
 
-    return formatter.format(TimeStampDate)
+        return formatter.format(TimeStampDate)
+    } catch (error) {
+        return error?.toString()
+    }
 }
