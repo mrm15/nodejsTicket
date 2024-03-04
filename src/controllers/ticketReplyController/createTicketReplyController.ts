86929d60339dd7efa73b1be47cf98ac2003e68b7
mrm15 <mrm.learn@gmail.com>
2024-03-04 12:49:09 +0330

    update createTicketReplyController.ts

--- src/controllers/ticketReplyController/createTicketReplyController.ts
+++ src/controllers/ticketReplyController/createTicketReplyController.ts
@@ -5,7 +5,7 @@ import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
 import {ITicketReply, TicketReply} from "../../models/ticketReply";
 import {getCurrentTimeStamp} from "../../utils/timing";
 import {ITicket, Ticket} from "../../models/ticket";
-import {User} from "../../models/User";
+import {IUser, User} from "../../models/User";
 import {Department} from "../../models/department";
 import {generateRefreshToken} from "../LoginRegisterSms/generateAccessToken";
 import {uuidGenerator} from "../../utils/uuidGenerator";
@@ -13,10 +13,78 @@ import {uuidGenerator} from "../../utils/uuidGenerator";
 
 const createTicketReplyController = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
 
-    const ticketId = (((await Ticket.findOne({}).exec())!)['_id']);
-    const userId = (((await User.findOne({}).exec())!)['_id']);
-    const departmentId = (((await Department.findOne({}).exec())!)['_id']);
-    const description = ' توضیحات تستی من  ' + uuidGenerator();
+
+    const {myToken} = req;
+    if (!myToken) {
+        const message = 'مقدار توکن توی ری کوئست موجود نیست'
+        res.status(403).json({message});
+        return
+    }
+
+
+    let {
+        ticketNumber,
+        description,
+        visibleToUser,
+        attachments,
+    } = req.body;
+
+
+    if (!visibleToUser) {
+
+        visibleToUser = true
+    }
+
+
+    if (!ticketNumber) {
+        res.status(500).json({
+            message: 'شماره تیکت  در ریکوئست موجود نیست',
+        });
+        return
+    }
+
+
+    if (!description) {
+        res.status(500).json({
+            message: 'مقدار توضیحات نمیتواند خالی باشد  ',
+        });
+        return
+    }
+    if (!attachments) {
+        attachments = []
+    }
+
+
+    const ticketId = (((await Ticket.findOne({ticketNumber: ticketNumber}).exec())!)['_id']);
+
+    const userId = myToken?.UserInfo?.userData?.userData?.userId
+
+
+    if (!userId) {
+        res.status(500).json({
+            message: 'مقدار userId نمیتواند خالی باشد  ',
+        });
+        return
+    }
+
+    //  دپارتمان فعلی که کاربر توش عضو هست رو وارد میکنم
+    const foundUser: IUser | null = await User.findOne({_id: userId}).lean()
+    if (!foundUser) {
+        res.status(500).json({
+            message: 'هیچ کاربری برای ثبت  دپارتمان یافت نشد'
+        });
+        return
+    }
+
+
+    const departmentId = foundUser?.departmentId
+    if (!departmentId) {
+        res.status(500).json({
+            message: 'دپارتمان کاربر نمیتواند خالی باشد'
+        });
+        return
+    }
+
 
     const dataToInsertInTicketReplyCollection = {
         ticketId,
@@ -24,23 +92,18 @@ const createTicketReplyController = async (req: CustomRequestMyTokenInJwt, res: 
         departmentId,
         description,
         replyDate: getCurrentTimeStamp(),
-        attachments: [],
+        attachments,
         visibleToUser: true,
         createAt: getCurrentTimeStamp(),
         updateAt: getCurrentTimeStamp(),
-
     }
-
-
 
 
     try {
 
         const result = await TicketReply.create(dataToInsertInTicketReplyCollection)
         res.status(200).json({
-            message: 'ظاهرا ثبت شد.',
-            dataToInsertInTicketReplyCollection,
-            result
+            message: 'پاسخ شما با موفقیت ثبت شد.',
         });
         return
     } catch (error) {
@@ -50,6 +113,7 @@ const createTicketReplyController = async (req: CustomRequestMyTokenInJwt, res: 
         });
         return
     }
+
 
 };
 
