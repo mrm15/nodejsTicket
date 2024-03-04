348439ceab014b2bab0f465d88458a77924c143d
mrm15 <mrm.learn@gmail.com>
2024-03-04 15:14:53 +0330

    update

--- src/index.ts
+++ src/index.ts
@@ -33,9 +33,7 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: true}));
 
 // Built-in middleware for JSON
-// app.use(express.json());
-// @ts-ignore
-app.use(express.json({ limit: '10mb', extended: true, encoding: 'utf-8' }));
+app.use(express.json());
 
 app.use(useragent.express());
 
