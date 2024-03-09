63a3d3d903151d47bd27e3950f355c30d8a81485
mrm15 <mrm.learn@gmail.com>
2024-03-09 10:47:28 +0330

    add define Table to readTicketController.ts

--- src/controllers/ticketController/readTicketController.ts
+++ src/controllers/ticketController/readTicketController.ts
@@ -6,6 +6,7 @@ import {checkAccessList} from "../../utils/checkAccessList";
 import {ITicket, Ticket} from "../../models/ticket";
 import {IUser, User} from "../../models/User";
 import {timestampToTime} from "../../utils/timestampToTime";
+import {defineTable} from "./defineTable";
 
 
 const readTicketController = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
@@ -26,11 +27,6 @@ const readTicketController = async (req: CustomRequestMyTokenInJwt, res: Respons
     }
 
 
-    // res.status(201).json({myToken})
-    //
-    // return
-
-
     try {
         const arrayListToCheck = [ACCESS_LIST.TICKET_READ_ALL]
         const hasAccessToReadAllTicket = await checkAccessList({phoneNumber: myToken.phoneNumber, arrayListToCheck})
@@ -39,54 +35,20 @@ const readTicketController = async (req: CustomRequestMyTokenInJwt, res: Respons
             return
         }
 
-        // اینجا قراره بعدا 10 تا 10 تا درخواست بزنم بگم هر ده تا یه بار درخواست بزن
-        const {startIndex , endIndex} = req.params;
 
-        let ticketList: ITicket[];
-        if(!startIndex || !endIndex){
-            ticketList= await Ticket.find({}).lean()
-        }else {
-            ticketList = await Ticket.find({}).lean()
-        }
-
-
-
-        const myList = await Promise.all(ticketList.map(async (singleTicket) => {
-            const row: any = {...singleTicket};
-            const userFound: IUser = (await User.findOne({_id: row.userId}).lean())!;
-            row['userCreateThisOrder'] = userFound.name
-            row['numberOfAttachments'] = row.attachments.length
-            row['dateCreate'] = timestampToTime(row.createAt)
-            row['lastChangeTime'] = timestampToTime(row.lastChangeTimeStamp)
-            return row;
-        }));
-
-
-
-
-        const columnDefs = []
-
-        columnDefs.push({minWidth: 150, headerName: "کد سفارش", field: "ticketNumber"})
-        columnDefs.push({minWidth: 150, headerName: "عنوان سفارش", field: "title"})
-        columnDefs.push({minWidth: 150, headerName: "توضیح", field: "description"})
-        columnDefs.push({minWidth: 150, headerName: "تاریخ ثبت ", field: "dateCreate"})
-        columnDefs.push({minWidth: 150, headerName: "تاریخ آخرین تغییر ", field: "lastChangeDate"})
-        columnDefs.push({minWidth: 150, headerName: "کاربر ثبت کننده سفارش", field: "userCreateThisOrder"})
-        columnDefs.push({minWidth: 150, headerName: "تعداد فایل ضمیمه ", field: "numberOfAttachments"})
-
-
-        const rowData = [...myList]
-
-        const list = {columnDefs, rowData}
+        const list = await defineTable({
+            req, conditionString: 'readTicketController',
+            ticketUserId: '',
+        })
 
         res.status(200).json({
             list, message: 'لیست بارگزاری شد.',
         });
         return;
 
-    } catch (error:any) {
+    } catch (error: any) {
 
-        res.status(500).json({error:error.toString()});
+        res.status(500).json({error: error.toString()});
         return
     }
 
