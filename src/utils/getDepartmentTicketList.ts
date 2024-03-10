b5c80f772f40668c7c9a479fc4a4ad1fc86ca7bc
mrm15 <mrm.learn@gmail.com>
2024-03-10 18:04:33 +0330

    update

--- src/utils/getDepartmentTicketList.ts
+++ src/utils/getDepartmentTicketList.ts
@@ -10,3 +10,27 @@ export const getDepartmentTicketList = async ({id}: myInterface) => {
     return ticketList || []
 }
 
+export const departmentTickets = async (id: myInterface) => {
+
+    const rowData = await getDepartmentTicketList(id)
+
+    const columnDefs = []
+
+    columnDefs.push({minWidth: 150, headerName: "کد سفارش", field: "ticketNumber"})
+    columnDefs.push({minWidth: 150, headerName: "عنوان سفارش", field: "title"})
+    columnDefs.push({minWidth: 150, headerName: "توضیح", field: "description"})
+    columnDefs.push({minWidth: 150, headerName: "تاریخ ثبت ", field: "dateCreate"})
+    columnDefs.push({minWidth: 150, headerName: "تاریخ آخرین تغییر ", field: "lastChangeDate"})
+    columnDefs.push({minWidth: 150, headerName: "کاربر ثبت کننده سفارش", field: "userCreateThisOrder"})
+    columnDefs.push({minWidth: 150, headerName: "تعداد فایل ضمیمه ", field: "numberOfAttachments"})
+
+
+    const list = {columnDefs, rowData}
+
+    return list
+
+
+}
+
+
+
