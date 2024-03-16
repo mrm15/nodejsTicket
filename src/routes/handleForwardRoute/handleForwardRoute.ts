4d454759f07b394c8411ce44241c908225be6e84
mrm15 <mrm.learn@gmail.com>
2024-03-16 15:20:08 +0330

    Create handleForwardRoute.ts

---
+++ src/routes/handleForwardRoute/handleForwardRoute.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import {forwardTicketToUserController} from "../../controllers/forwardTicketController/forwardTicketToUserController";
+import {readTicketReplyController} from "../../controllers/forwardTicketController/readTicketReplyController";
+import {updateTicketReplyController} from "../../controllers/forwardTicketController/updateTicketReplyController";
+import {deleteTicketReplyController} from "../../controllers/forwardTicketController/deleteTicketReplyController";
+import {ticketReplyList} from "../../controllers/forwardTicketController/ticketReplyList";
+
+
+const router = express.Router();
+
+router.post('/toUser', forwardTicketToUserController)  //  // forward  to user
+router.post('/toDepartment', readTicketReplyController) // // forward ticket  to Department
+
+router.post('/toUserToDepartment', readTicketReplyController) // // forward ticket  to  a user in a Department
+
+router.post('/update', updateTicketReplyController)
+router.delete('/delete/:id', deleteTicketReplyController)
+router.get('/replyList', ticketReplyList);
+
+export default router;
