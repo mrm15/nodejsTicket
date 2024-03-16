b8c1e2b5d3d7dd08fb684b4d872fcd43ac2141fc
mrm15 <mrm.learn@gmail.com>
2024-03-16 14:23:28 +0330

    Update userTicketTable.ts

--- src/utils/userTicketTable.ts
+++ src/utils/userTicketTable.ts
@@ -1,6 +1,7 @@
 import {IUser, User} from "../models/User";
 import {ITicket, Ticket} from "../models/ticket";
 import mongoose from "mongoose";
+import {Department, IDepartment} from "../models/department";
 
 interface myObject {
     userId: mongoose.Schema.Types.ObjectId;
@@ -21,23 +22,24 @@ export const userTicketTable = async ({userId}: myObject) => {
 
         const foundTicket: ITicket = (await Ticket.findOne({_id: ticketId}).lean())!;
 
-        // row['userCreateThisOrder'] = userFound.name
-        // row['numberOfAttachments'] = row.attachments.length
-        // row['dateCreate'] = timestampToTime(row.createAt)
-        // row['lastChangeTime'] = timestampToTime(row.lastChangeTimeStamp)
+        const row: any = {...foundTicket}
 
-        // const {_v, ...rest} = foundTicket
-        // return rest;
-        return foundTicket
+        const lastDepartmentAssigned: IDepartment | null = await Department.findOne({_id: foundTicket.assignedToDepartmentId})
+        row['lastDepartmentAssigned'] = lastDepartmentAssigned ? lastDepartmentAssigned?.name : ''
 
-    }));
+        const lastUserAssigned: IDepartment | null = await Department.findOne({_id: foundTicket.assignToUserId})
+        row['lastUserAssigned'] = lastUserAssigned ? lastUserAssigned.name : ''
+
+        return row
+    }))
 
 
     const columnDefs = []
 
     columnDefs.push({minWidth: 150, headerName: "کد سفارش", field: "ticketNumber"})
     columnDefs.push({minWidth: 150, headerName: "عنوان سفارش", field: "title"})
-    columnDefs.push({minWidth: 150, headerName: "توضیح", field: "description"})
+    columnDefs.push({minWidth: 150, headerName: "آخرین دپارتمان", field: "lastDepartmentAssigned"})
+    columnDefs.push({minWidth: 150, headerName: "آخرین کاربر", field: "lastUserAssigned"})
     columnDefs.push({minWidth: 150, headerName: "تاریخ ثبت ", field: "dateCreate"})
     columnDefs.push({minWidth: 150, headerName: "تاریخ آخرین تغییر ", field: "lastChangeDate"})
     columnDefs.push({minWidth: 150, headerName: "کاربر ثبت کننده سفارش", field: "userCreateThisOrder"})
