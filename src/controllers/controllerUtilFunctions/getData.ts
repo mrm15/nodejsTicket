aeba2c8b434fea0e79442b2063170b82b3c8ecf0
mrm15 <mrm.learn@gmail.com>
2024-04-04 10:28:02 +0330

    add departmentId   and fix  seprate by departments

--- src/controllers/controllerUtilFunctions/getData.ts
+++ src/controllers/controllerUtilFunctions/getData.ts
@@ -1,20 +1,20 @@
 import {IUser, User} from "../../models/User";
 import {Department, IDepartment} from "../../models/department";
+import {IDataList} from "../forwardTicketController/getForwardConfig";
 
 export const getDepartmentListWithUsers = async (phoneNumber: string) => {
 
-
-    const allUsers = await User.find({}).lean();
+    const allUsers : IUser[] = await User.find({}).lean();
     const allDepartments: IDepartment[] | [] = await Department.find({}).lean()
 
 
     return allDepartments.map(singleDepartment => {
         const depName = singleDepartment.name;
         const depId = singleDepartment._id;
-        const userList = allUsers.filter(singleUser => singleUser.departmentId === singleDepartment._id)
-
-        return {name: depName, id: depId, userList}
-
+        const userList = allUsers
+            .filter(singleUser => singleUser.departmentId?.toString() === singleDepartment._id.toString())
+            .map(singleUser => ({id: singleUser._id, name: singleUser.name ,departmentId :singleUser.departmentId?.toString() }));
+        return {name: depName, id: depId, userList  }
     })
 }
 export const getSameDepartmentUsers = async (phoneNumber: string) => {
