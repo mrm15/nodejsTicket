ba0b5425f40e321542cf1e3ce12a42015a8913bd
mrm15 <mrm.learn@gmail.com>
2024-04-21 14:46:06 +0330

    Update handleTicketRoute.ts

--- src/routes/handleTicketRoute/handleTicketRoute.ts
+++ src/routes/handleTicketRoute/handleTicketRoute.ts
@@ -16,8 +16,8 @@ router.post('/create', createTicketController);
 
 router.get('/read', readTicketController); // a simple get All data controller for ADMIN
 router.get('/readSentTickets', ReadSentTicketController); // sent ticket List // done
-router.get('/readMyAllTickets', readMyAllTicketsController); // done tickets that is im my department and in my ticketAccess List in user Model
-router.get('/readMyInboxTickets', readMyInboxTicketsController); //  tickets that is im my department and in my ticketAccess List in user Model
+router.get('/readMyAllTickets', readMyAllTicketsController); // فقط تیکت هایی که در یوزر مدل قرار داره رو بفرست انجام شده
+router.get('/readMyInboxTickets', readMyInboxTicketsController); //  مشترک بین دپارتمان و یورز که در یوزر مدل قرار داره
 router.get('/readDepartmentTickets', readDepartmentTicketsController); //  tickets that is im my department  just admin of department has access to this part
 
 router.get('/chatList/:ticketId', chatListTicketController);
