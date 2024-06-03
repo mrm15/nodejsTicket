159d0b6c9861e309302e093731f20b6c064df844
mrm15 <mrm.learn@gmail.com>
2024-06-03 22:23:52 +0330

    add pending routes

--- src/routes/handleSmsRoute/handleSmsRoute.ts
+++ src/routes/handleSmsRoute/handleSmsRoute.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import {createSmsTask} from "../../controllers/smsController/createSmsTask";
 import {getArchiveController} from "../../controllers/smsController/getArchiveController";
+import {getPendingController} from "../../controllers/smsController/getPendingController";
 
 
 const router = express.Router();
 
 router.post('/create', createSmsTask)
 router.get('/getArchive', getArchiveController)
-// router.get('/getPending', getPendingController)
+router.get('/getPending', getPendingController)
 
 
 export default router;
