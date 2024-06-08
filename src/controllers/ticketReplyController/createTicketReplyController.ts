ecb25e5fd1a74bfd52243cc183e9654f0bd06db3
mrm15 <mrm.learn@gmail.com>
2024-06-08 12:49:56 +0330

    Update createTicketReplyController.ts

--- src/controllers/ticketReplyController/createTicketReplyController.ts
+++ src/controllers/ticketReplyController/createTicketReplyController.ts
@@ -6,9 +6,11 @@ import {ITicketReply, TicketReply} from "../../models/ticketReply";
 import {getCurrentTimeStamp} from "../../utils/timing";
 import {ITicket, Ticket} from "../../models/ticket";
 import {IUser, User} from "../../models/User";
-import {Department} from "../../models/department";
+import {Department, IDepartment} from "../../models/department";
 import {generateRefreshToken} from "../LoginRegisterSms/generateAccessToken";
 import {uuidGenerator} from "../../utils/uuidGenerator";
+import {setForSendMessage} from "../../utils/setForSendMessage";
+import {logger} from "../../middleware/logEvents";
 
 
 const createTicketReplyController = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
@@ -30,8 +32,7 @@ const createTicketReplyController = async (req: CustomRequestMyTokenInJwt, res: 
     } = req.body;
 
 
-
-    if (visibleToUser=== undefined) {
+    if (visibleToUser === undefined) {
         visibleToUser = true
     }
 
@@ -55,10 +56,20 @@ const createTicketReplyController = async (req: CustomRequestMyTokenInJwt, res: 
     }
 
 
-    const ticketId = (((await Ticket.findOne({ticketNumber: ticketNumber}).exec())!)['_id']);
+    let smsMsg = "";
+    const ticketDoc = (await Ticket.findOne({ticketNumber: ticketNumber}).exec())!
+    const ticketId = ticketDoc['_id'];
 
     const userId = myToken?.UserInfo?.userData?.userData?.userId
-
+    // maye user that create this ticket  has been deleted or deactive
+    const customerUserId = ticketDoc.userId;
+    const customerDocument: IUser | null = await User.findOne({_id: customerUserId.toString()});
+    if (customerDocument === null) {
+        res.status(500).json({
+            message: 'کاربر سفارش دهنده حذف شده و امکان ارسال پیام وجود ندارد.',
+        })
+        return
+    }
 
     if (!userId) {
         res.status(500).json({
@@ -84,6 +95,51 @@ const createTicketReplyController = async (req: CustomRequestMyTokenInJwt, res: 
         });
         return
     }
+    // بیا چک کنیم ببینم لازمه اس ام اس بدیم یا نه؟
+    const foundUserDepartment: IDepartment | null = await Department.findOne({_id: departmentId})
+    if (!foundUserDepartment) {
+        res.status(500).json({
+            message: 'کاربر در هیچ دپارتمانی نیست.',
+        });
+    }
+
+
+    debugger
+    // برای قطعی شدن اسراسل اس ام اس باید حتما بدونیم که پیام مخفی نیست
+
+    if (visibleToUser) {
+
+
+        const shouldISendSms = foundUserDepartment?.sendSmsAfterSubmitResponse;
+        if (shouldISendSms) {
+            const smsText = foundUserDepartment?.smsText;
+            if (!smsText) {
+                res.status(500).json({
+                    message: 'در تنطیمات پیامک فعال است ولی متنی نداریم. لطفا با مدیر سایت تماس بگیرید.',
+                });
+                return;
+            }
+            // send SMS
+            try {
+
+                const customerPhoneNumber = customerDocument.phoneNumber
+                const depString = departmentId?.toString();
+                const resultSendSms = await setForSendMessage({
+                    senderUserId: userId, senderDepartmentId: depString, text: smsText, replyId: null,
+                    destinationNumber: customerPhoneNumber
+                });
+                if (resultSendSms.smsStatusCode===200) {
+                    smsMsg = "پیامک درج شد."
+                }
+
+
+            } catch (error) {
+                console.log(error)
+            }
+
+
+        }
+    }
 
 
     const dataToInsertInTicketReplyCollection = {
@@ -101,9 +157,10 @@ const createTicketReplyController = async (req: CustomRequestMyTokenInJwt, res: 
 
     try {
 
+        const message = 'پاسخ شما با موفقیت ثبت شد.' + " " + smsMsg
         const result = await TicketReply.create(dataToInsertInTicketReplyCollection)
         res.status(200).json({
-            message: 'پاسخ شما با موفقیت ثبت شد.',
+            message,
         });
         return
     } catch (error) {
