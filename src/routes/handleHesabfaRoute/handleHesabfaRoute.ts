5bb3f9a989e1cd41c1cdb07de40716972caef3f3
mrm15 <mrm.learn@gmail.com>
2024-06-30 17:47:31 +0330

    Update handleHesabfaRoute.ts

--- src/routes/handleHesabfaRoute/handleHesabfaRoute.ts
+++ src/routes/handleHesabfaRoute/handleHesabfaRoute.ts
@@ -3,6 +3,7 @@ import {getAllProducts} from "../../controllers/hesabfaController/getAllProducts
 import {getAllProjects} from "../../controllers/hesabfaController/getAllProjects";
 import {getBillData} from "../../controllers/hesabfaController/getBillData";
 import {getCustomerList} from "../../controllers/hesabfaController/getCustomerList";
+import {submitBillInHesabfa} from "../../controllers/hesabfaController/submitBillInHesabfa";
 
 
 const router = express.Router();
@@ -11,6 +12,7 @@ router.get('/getAllProducts', getAllProducts)
 router.get('/getAllProjects', getAllProjects)
 router.get('/getBillData/:billNumber', getBillData)
 router.get('/getCustomerList/', getCustomerList)
+router.post('/submitBill/', submitBillInHesabfa)
 // router.get('/getArchive', getArchiveController)
 // router.get('/getPending', getPendingController)
 
