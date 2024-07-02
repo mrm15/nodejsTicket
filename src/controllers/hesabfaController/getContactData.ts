c76d0e873993ecc175ea9c87d0d20939d6ad0fad
mrm15 <mrm.learn@gmail.com>
2024-07-02 23:36:48 +0330

    add getContactData

---
+++ src/controllers/hesabfaController/getContactData.ts
@@ -0,0 +1,99 @@
+import {NextFunction, Response} from 'express';
+import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
+import {ACCESS_LIST} from "../../utils/ACCESS_LIST";
+import {checkAccessList} from "../../utils/checkAccessList";
+import {IUser, User} from "../../models/User";
+import {setForSendMessage} from "../../utils/setForSendMessage";
+import axios from "axios";
+import {handleResponse} from "../utility/handleResponse";
+
+
+const getContactData = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
+
+    const {contactCode} =  req.params;
+
+    console.log(contactCode)
+    debugger
+    const API_KEY = process.env.HESABFA_API_KEY
+    if(!API_KEY){
+        res.status(500).json({message:'api key یافت نشد'});
+        return
+    }
+    const LOGIN_TOKEN = process.env.HESABFA_LOGIN_TOKEN
+    if(!LOGIN_TOKEN){
+        res.status(500).json({message:'LOGIN TOKEN یافت نشد'});
+        return
+    }
+
+    if(!contactCode){
+        const message = 'مقدار شماره مشتری معتبر نیست'
+        res.status(500).json({message});
+        return
+    }
+
+    const {myToken} = req;
+    if (!myToken) {
+        const message = 'مقدار توکن توی ری کوئست موجود نیست'
+        res.status(200).json({message});
+        return
+    }
+
+
+    try {
+
+
+        // const arrayListToCheck = [
+        //     ACCESS_LIST.SMS_SEND
+        // ]
+        // const hasAccessTo = await checkAccessList({phoneNumber: myToken.phoneNumber, arrayListToCheck})
+        //
+        // if (!hasAccessTo) {
+        //     res.status(403).json({message: 'شما مجوز دسترسی به این بخش را ندارید.'})
+        //     return
+        // }
+
+
+        const {userId} = myToken?.UserInfo?.userData?.userData;
+
+        const foundUser: IUser | null = await User.findOne({_id: userId}).lean();
+
+
+        if (!foundUser) {
+            res.status(401).json({message: 'کاربری با این شماره تلفن یافت نشد'});
+            return
+        }
+
+
+        // دریافت تمام محصولات از حسابفا
+
+        try {
+            const url = 'https://api.hesabfa.com/v1/contact/get'
+            const data = {
+                apiKey: API_KEY,
+                // userId: 'mail@example.com',
+                // password: '123456',
+                loginToken: LOGIN_TOKEN,
+                code: contactCode + "",
+            }
+            const result = await axios.post(url, data);
+            debugger
+            console.log(result)
+            handleResponse(result,res)
+        } catch (error:any) {
+            const statusCode = error?.status || 500
+            res.status(statusCode).json({
+                message: error?.toString(),
+            })
+            return;
+        }
+
+    } catch (error) {
+
+        res.status(500).json({error});
+        return
+    }
+
+
+};
+
+export {getContactData};
--- src/routes/handleHesabfaRoute/handleHesabfaRoute.ts
+++ src/routes/handleHesabfaRoute/handleHesabfaRoute.ts
@@ -4,6 +4,7 @@ import {getAllProjects} from "../../controllers/hesabfaController/getAllProjects
 import {getBillData} from "../../controllers/hesabfaController/getBillData";
 import {getCustomerList} from "../../controllers/hesabfaController/getCustomerList";
 import {submitBillInHesabfa} from "../../controllers/hesabfaController/submitBillInHesabfa";
+import {getContactData} from "../../controllers/hesabfaController/getContactData";
 
 
 const router = express.Router();
@@ -12,6 +13,7 @@ router.get('/getAllProducts', getAllProducts)
 router.get('/getAllProjects', getAllProjects)
 router.get('/getBillData/:billNumber', getBillData)
 router.get('/getCustomerList/', getCustomerList)
+router.get('/getContactData/:contactCode', getContactData)
 router.post('/submitBill/', submitBillInHesabfa)
 // router.get('/getArchive', getArchiveController)
 // router.get('/getPending', getPendingController)
