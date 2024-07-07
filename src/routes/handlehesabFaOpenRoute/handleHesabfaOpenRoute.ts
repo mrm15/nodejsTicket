8d64c7c3895f219dc9c6448c5ac0f0471a6fa9a8
mrm15 <mrm.learn@gmail.com>
2024-07-07 15:44:15 +0330

    add route  saveAllContactsWithPhoneNumber

--- src/routes/handlehesabFaOpenRoute/handleHesabfaOpenRoute.ts
+++ src/routes/handlehesabFaOpenRoute/handleHesabfaOpenRoute.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import {getBillData} from "../../controllers/hesabfaOpenController/getBillData";
+import {saveAllContactsWithPhoneNumber} from "../../controllers/hesabfaOpenController/saveAllContactsWithPhoneNumber";
 
 
 const router = express.Router();
 
 
 router.get('/getBillData/:billNumber', getBillData)
+router.get('/saveAllContactsWithPhoneNumber', saveAllContactsWithPhoneNumber)
 
 // router.get('/getArchive', getArchiveController)
 // router.get('/getPending', getPendingController)
