73bd3fd344f683e86ffdea06a17a1322eab6bebf
mrm15 <mrm.learn@gmail.com>
2024-07-09 13:03:24 +0330

    deleteBillInHesabfaController Route

--- src/routes/handleHesabfaRoute/handleHesabfaRoute.ts
+++ src/routes/handleHesabfaRoute/handleHesabfaRoute.ts
@@ -5,6 +5,7 @@ import {getBillData} from "../../controllers/hesabfaController/getBillData";
 import {getCustomerList} from "../../controllers/hesabfaController/getCustomerList";
 import {submitBillInHesabfa} from "../../controllers/hesabfaController/submitBillInHesabfa";
 import {getContactData} from "../../controllers/hesabfaController/getContactData";
+import {deleteBillInHesabfaController} from "../../controllers/hesabfaController/deleteBillInHesabfaController";
 
 
 const router = express.Router();
@@ -15,6 +16,7 @@ router.get('/getBillData/:billNumber', getBillData)
 router.get('/getCustomerList/', getCustomerList)
 router.get('/getContactData/:contactCode', getContactData)
 router.post('/submitBill/', submitBillInHesabfa)
+router.get('/deleteBill/:billNumber/:type/:id', deleteBillInHesabfaController)
 // router.get('/getArchive', getArchiveController)
 // router.get('/getPending', getPendingController)
 
