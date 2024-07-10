df9f0e9b1b195ab0e0a14e6d33d4ef1aaeac9eed
mrm15 <mrm.learn@gmail.com>
2024-07-10 15:58:25 +0330

    Create template.ts

---
+++ src/SMS/template.ts
@@ -0,0 +1,6 @@
+export const afterSubmitBillSmsText = {
+    text:`کاربر عزیز customerName یک فاکتور در سیستم فروش نمارنگ برای شما ثبت شد از لینک زیر فاکتور خود را مشاهده کنید.
+    billLink
+    `,
+    replaceItems:["customerName","billLink"]
+}
