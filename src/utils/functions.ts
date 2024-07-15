e0314a6010ff17e5db2b0b14ef49892878d5d028
mrm15 <mrm.learn@gmail.com>
2024-07-16 00:52:16 +0330

    Update functions.ts

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,1 +1,11 @@
 export const setNullIfEmpty = (value: string | null | undefined) => (value === '' ? null : value);
+
+export const getProductListFromFile=()=>{
+
+    const fileAddress = "./productList.json";
+
+
+
+
+
+}
