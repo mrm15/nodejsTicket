8f0c97b67ab2b9e1d40fd6bd53a04b077284ff60
mrm15 <mrm.learn@gmail.com>
2024-07-28 09:28:35 +0330

    Update handleTicketRoute.ts

--- src/routes/handleTicketRoute/handleTicketRoute.ts
+++ src/routes/handleTicketRoute/handleTicketRoute.ts
@@ -14,11 +14,11 @@ const router = express.Router();
 
 router.post('/create', createTicketController);
 
-router.get('/read', readTicketController); // a simple get All data controller for ADMIN
-router.get('/readSentTickets', ReadSentTicketController); // sent ticket List // done
-router.get('/readMyAllTickets', readMyAllTicketsController); // فقط تیکت هایی که در یوزر مدل قرار داره رو بفرست انجام شده
-router.get('/readMyInboxTickets', readMyInboxTicketsController); //  مشترک بین دپارتمان و یورز که در یوزر مدل قرار داره
-router.get('/readDepartmentTickets', readDepartmentTicketsController); //  tickets that is im my department  just admin of department has access to this part
+router.get('/read', readTicketController); // همه ی تیکت های سیستم رو یکجا میده
+router.get('/readSentTickets', ReadSentTicketController); // تیکت های ارسال من رو میره از توی کل تیکت ها میکشه بیرون و نشون میده
+router.get('/readMyAllTickets', readMyAllTicketsController)// done works correctly; // فقط تیکت هایی که در یوزر مدل قرار داره رو بفرست انجام شده
+router.get('/readMyInboxTickets', readMyInboxTicketsController);//done works correctly; //  مشترک بین دپارتمان و یورز که در یوزر مدل قرار داره
+router.get('/readDepartmentTickets', readDepartmentTicketsController); // تیکت های  دپارتمان که فقط ادمین دپارتمان بهشون دسترسی داره  tickets that is im my department  just admin of department has access to this part
 
 router.get('/chatList/:ticketId', chatListTicketController);
 router.post('/update', updateTicketController);
