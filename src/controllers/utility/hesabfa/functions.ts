9be8f94a609cc2af3b8b57ad33013ad3e1af6253
mrm15 <mrm.learn@gmail.com>
2024-07-29 19:05:09 +0330

    Update functions.ts

--- src/controllers/utility/hesabfa/functions.ts
+++ src/controllers/utility/hesabfa/functions.ts
@@ -60,6 +60,120 @@ export const getHeaderAndRows = (incomeData: any) => {
         const myContactName = row?.Contact?.Name
         const myContactCode = row.ContactCode
         const myContactTitle = row.ContactTitle
+        dataSheet2.push({
+            myContactName,
+            myContactCode,
+            myContactTitle,
+            myDate:row.Date,
+            myTotalAmount : row.Sum,
+            myStatus:row.Status,
+        })
+
+        // row?.InvoiceItems.forEach((rowItem: any) => {
+        //     dataSheet2.push({
+        //         // ...row,
+        //         // ...rowItem,
+        //         myNumber: row.Number,
+        //         myDate: row.Date,
+        //         myContactCode,
+        //         myContactName,
+        //         myContactTitle,
+        //         myItemCode: rowItem?.Item?.Code,
+        //         myItemName: rowItem?.Item?.Name,
+        //         myDescription: rowItem?.Description,
+        //         myUnit: rowItem?.Unit,
+        //         myQuantity: rowItem?.Quantity,
+        //         myUnitPrice: rowItem?.UnitPrice,
+        //         myDiscount: rowItem?.Discount,
+        //         mySeller,
+        //         myTicketNumber,
+        //         myTax: rowItem?.Tax,
+        //         myTotalAmount: rowItem?.TotalAmount, // اینحا ما جمع مربوط به همون آیتم رو فقط لازم داریم. نه جمع کل
+        //         myStatus: row.Status,
+        //         myCurrency: row.Currency,
+        //     })
+        // })
+    })
+
+
+    const headersSheet2 = [
+        {title: "شماره فاکتور", key: "myNumber"},
+        {title: "تاریخ", key: "myDate", task: DateToEnglish},
+        {title: "کد شخص", key: "myContactCode"},
+        {title: "نام شخص", key: "myContactName"},
+        {title: "عنوان", key: "myContactTitle"},
+        {title: "کد کالا", key: "myItemCode"},
+        {title: "کالا", key: "myItemName"},
+        {title: "شرح", key: "myDescription"},
+        {title: "واحد", key: "myUnit"},
+        {title: "تعداد", key: "myQuantity"},
+        {title: "مبلغ واحد", key: "myUnitPrice"},
+        {title: "تخفیف", key: "myDiscount"},
+        {title: "فروشنده", key: "mySeller"},
+        {title: "شماره سفارش", key: "myTicketNumber"},
+        {title: "مالیات", key: "Tax"},
+        {title: "مبلغ کل", key: "TotalAmount"},
+        {title: "واحد پول", key: "myCurrency"},
+        {title: "وضعیت", key: "myStatus", task: changeTextTo},
+    ]
+
+
+    const formattedDataSheet2 = dataSheet2.map((item: any) => {
+        const newItem: { [key: string]: any } = {};
+        headersSheet2.forEach((header) => {
+            newItem[header.title] = header.task ? header.task((item)[header.key]) : (item)[header.key]
+        })
+        return newItem;
+    })
+
+
+    return {
+        header: headersSheet2,
+        rows: dataSheet2,
+    }
+
+}
+
+export const getHeaderAndRowsDetails = (incomeData: any) => {
+
+    const DateToEnglish = (inputText: any) => {
+        let result = timestampToTimeFromHesabfa(inputText)
+        result = p2e(result);
+        return result
+    }
+    //
+
+    const changeTextTo = (inputNumber: any) => {
+
+        return inputNumber === 0 ? "پیشنویس" : "تایید شده"
+    }
+    //
+
+
+    const dataSheet2: any = []
+
+    incomeData.forEach((row: any) => {
+        let myTicketNumber = ""
+        let mySeller = ""
+        try {
+            myTicketNumber = JSON.parse(row?.Tag)?.tn || "";
+            mySeller = JSON.parse(row?.Tag)?.n || "";
+        } catch (error) {
+            myTicketNumber = ""
+            mySeller = ""
+        }
+
+        const myContactName = row?.Contact?.Name
+        const myContactCode = row.ContactCode
+        const myContactTitle = row.ContactTitle
+        // dataSheet2.push({
+        //     myContactName,
+        //     myContactCode,
+        //     myContactTitle,
+        //     myDate:row.Date,
+        //     myTotalAmount : row.Sum,
+        //     myStatus:row.Status,
+        // })
 
         row?.InvoiceItems.forEach((rowItem: any) => {
             dataSheet2.push({
@@ -121,38 +235,30 @@ export const getHeaderAndRows = (incomeData: any) => {
 
     return {
         header: headersSheet2,
-        rows: formattedDataSheet2,
+        rows: dataSheet2,
     }
 
 }
 
-export const filterResultDateStatus = (tempBillList = [], date: string) => {
+export const countFilterResultDateStatus = (tempBillList = [], date: string) => {
+    const filteredBills = tempBillList.filter((row: any) => row.myDate === date);
 
-
-    const verifiedBillTotalSum = 0
-    const verifiedBillsNumber = 0
-    const data = {
-        submittedBillTotalSum: 0,
-        submittedBillsNumber: 0,
-        verifiedBillTotalSum: 0,
-        verifiedBillsNumber: 0,
-    }
-
-    tempBillList.forEach((row: any) => {
-
-        if (row.Date === date) {
-            if (row.Status === 0) {
-                data.submittedBillsNumber += 1;
-                data.submittedBillTotalSum += row.TotalSum;
-
-            } else if (row.Status === 0) {
-                data.verifiedBillTotalSum += 1;
-                data.verifiedBillsNumber += row.TotalSum;
-            }
+    const data = filteredBills.reduce((acc, row: any) => {
+        if (row.myStatus === 0) {
+            acc.submittedBillsNumber = +acc.submittedBillsNumber + 1;
+            acc.submittedBillTotalSum = +acc.submittedBillTotalSum + (+row.myTotalAmount);
+        } else if (row.myStatus === 1) {
+            acc.verifiedBillsNumber = +acc.verifiedBillsNumber + 1;
+            acc.verifiedBillTotalSum = +acc.verifiedBillTotalSum + +row.myTotalAmount;
         }
-    })
+        return acc;
+    }, {
+        submittedBillsNumber: 0,
+        submittedBillTotalSum: 0,
+        verifiedBillsNumber: 0,
+        verifiedBillTotalSum: 0,
+    });
 
-    return data
-
-
+    debugger
+    return data;
 }
