5f420632f72602cba765f9a32ae8cd26a21b5525
mrm15 <mrm.learn@gmail.com>
2024-07-29 19:04:42 +0330

    Update number.ts

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -8,4 +8,5 @@ const loginCodeGenerator = (): number => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+export const formatNumber = (inp:number) =>inp.toLocaleString('en-US')
 export { generateLoginSms, loginCodeGenerator };
