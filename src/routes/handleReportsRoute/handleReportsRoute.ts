3a1c73d4adbfc68c91e9171cc8732c908f2d9eff
mrm15 <mrm.learn@gmail.com>
2024-07-29 19:04:53 +0330

    Update handleReportsRoute.ts

--- src/routes/handleReportsRoute/handleReportsRoute.ts
+++ src/routes/handleReportsRoute/handleReportsRoute.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import {getDashboardController} from "../../controllers/reportsController/getDashboardController";
+import {getTodayReportSms} from "../../controllers/reportsController/getTodayReportSms";
 
 
 
 const router = express.Router();
 
 router.get('/dashboard', getDashboardController)
+router.get('/todayReportSms', getTodayReportSms)
 
 
 export default router;
