e590cd2611b8d020178a6ed182a71ab4a3110bed
mrm15 <mrm.learn@gmail.com>
2024-07-31 15:29:06 +0330

    Update dbConn.ts

--- src/config/dbConn.ts
+++ src/config/dbConn.ts
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 
 const MONGO_USER = process.env.MONGODB_USER;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+const MONGODB_PORT = process.env.MONGODB_PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGO_USER) {
     throw new Error('MONGO_USER is not defined');
@@ -12,14 +14,20 @@ if (!MONGO_USER) {
 if (!MONGODB_PASSWORD) {
     throw new Error('MONGODB_PASSWORD is not defined');
 }
+if (!MONGODB_PORT) {
+    throw new Error('MONGODB_PORT is not defined');
+}
+if (!MONGODB_URI) {
+    throw new Error('MONGODB_PORT is not defined');
+}
 
 const dbName = 'ticket'; // Replace 'ticket' with your actual database name
 
 
-const mongoURI = 'mongodb://127.0.0.1:27017/ticket';
-// const mongoURI = `mongodb://${MONGO_USER}:${MONGODB_PASSWORD}@services.irn5.chabokan.net:36399/${dbName}?authSource=admin`;
+// const mongoURI = 'mongodb://services.irn4.chabokan.net:28192/ticket';
+const mongoURI = `mongodb://${MONGO_USER}:${MONGODB_PASSWORD}@${MONGODB_URI}:${MONGODB_PORT}/${dbName}?authSource=admin`;
 //mongodb://USERNASME:PASSWORD@MONGO_SERVER_URL:MONGO_PORT/?authSource=admin
-
+// console.log(mongoURI)
 const connectDB = async (): Promise<void> => {
     try {
         await mongoose.connect(mongoURI, {
