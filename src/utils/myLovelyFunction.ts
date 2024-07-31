a9dd722778ce9abe45ee39d39a861c3b0d7afa7b
mrm15 <mrm.learn@gmail.com>
2024-08-01 00:36:34 +0330

    Create myLovelyFunction.ts

---
+++ src/utils/myLovelyFunction.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import {IUser, User} from "../models/User";
+
+export const myLovelyFunction = async ()=>{
+
+
+    const allUsers : IUser[]  | null= await User.find({}).lean();
+    if(allUsers){
+
+        console.log("============================")
+        console.log(allUsers.length)
+        console.log("============================")
+
+    }else{
+        console.log("============================")
+        console.log("Done");
+    }
+
+
+
+}
