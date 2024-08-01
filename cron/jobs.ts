90596e61f271ed2b8afd6859918b46182350f4ec
mrm15 <mrm.learn@gmail.com>
2024-08-01 12:08:51 +0330

    Update jobs.ts

--- cron/jobs.ts
+++ cron/jobs.ts
@@ -1,29 +1,47 @@
 // cronJobs.ts
 import cron from 'node-cron';
 import { myLovelyFunction } from "../src/utils/myLovelyFunction";
+import {sendReportDaySMSToSomeOfUsers} from "../src/utils/cronFunctions/sendReportDaySMSToSomeOfUsers";
 
 // Function to initialize cron jobs
 export const initializeCronJobs = () => {
-    // Task to run every minute
-    cron.schedule('* * * * *', () => {
-        console.log('Running a task every minute');
-    });
+    // // Task to run every minute
+    // cron.schedule('* * * * *', () => {
+    //     console.log('Running a task every minute');
+    // });
 
-    // Task to run every day at midnight
-    cron.schedule('0 0 * * *', () => {
-        console.log('Running a task every day at midnight');
-        // Example: Fetch data from an API and log it
-    });
+    // // Task to run every day at midnight
+    // cron.schedule('0 0 * * *', () => {
+    //     console.log('Running a task every day at midnight');
+    //     // Example: Fetch data from an API and log it
+    // });
 
-    // Task to run every Monday at 9 AM
-    cron.schedule('0 9 * * 1', () => {
-        console.log('Running a task every Monday at 9 AM');
-    });
+    // // Task to run every Monday at 9 AM
+    // cron.schedule('0 9 * * 1', () => {
+    //     console.log('Running a task every Monday at 9 AM');
+    // });
 
     // Task to run every 10 seconds
-    cron.schedule('*/10 * * * * *', async () => {
-        console.log('Running a task every 10 seconds');
-        debugger
-        await myLovelyFunction();
+    // cron.schedule('*/10 * * * * *', async () => {
+    //     console.log('Running a task every 10 seconds');
+    //     debugger
+    //     await myLovelyFunction();
+    // });
+    // Schedule the job to run at 5 PM from Saturday to Thursday
+    cron.schedule('0 17 * * 0-4,6', async () => {
+        console.log('Running task every day at 5 PM, except Fridays');
+        await sendReportDaySMSToSomeOfUsers();
     });
+
+    // Schedule the job to run at 2 PM every Thursday
+    cron.schedule('0 13 * * 4', async () => {
+        await sendReportDaySMSToSomeOfUsers();
+    });
+
+    // Schedule the job to run at 5 PM from Saturday to Wednesday
+    cron.schedule('0 17 * * 0-3,6', async () => {
+        await sendReportDaySMSToSomeOfUsers();
+    });
+
+
 };
