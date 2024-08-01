0249e6788d1c0237584114dfb2f54848d22ce559
mrm15 <mrm.learn@gmail.com>
2024-08-01 12:09:10 +0330

    Update logEvents.ts

--- src/middleware/logEvents.ts
+++ src/middleware/logEvents.ts
@@ -4,21 +4,9 @@ import * as fs from 'fs';
 import { promises as fsPromises } from 'fs';
 import * as path from 'path';
 import { Request, Response, NextFunction } from 'express';
+import {logEvents} from "../utils/logEvents";
 
-const logEvents = async (message: string, logName: string): Promise<void> => {
-    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
-    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
-    try {
-        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
-        }
-
-        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
-    } catch (err) {
-
-    }
-};
 
 const logger = (req: Request, res: Response, next: NextFunction): void => {
     logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt').then((r) => {
