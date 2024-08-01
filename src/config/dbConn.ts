2e80c3491816f12628967bf4a42ca8b4184eff64
mrm15 <mrm.learn@gmail.com>
2024-08-01 12:09:29 +0330

    Update dbConn.ts

--- src/config/dbConn.ts
+++ src/config/dbConn.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 
-
 const MONGO_USER = process.env.MONGODB_USER;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 const MONGODB_PORT = process.env.MONGODB_PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
+const PROGRAM_MODE = process.env.PROGRAM_MODE;
 
 if (!MONGO_USER) {
     throw new Error('MONGO_USER is not defined');
@@ -24,13 +24,20 @@ if (!MONGODB_URI) {
 const dbName = 'ticket'; // Replace 'ticket' with your actual database name
 
 
-const mongoURI = 'mongodb://127.0.0.1:27017/ticket';
-// const mongoURI = `mongodb://${MONGO_USER}:${MONGODB_PASSWORD}@${MONGODB_URI}:${MONGODB_PORT}/${dbName}?authSource=admin`;
-//mongodb://USERNASME:PASSWORD@MONGO_SERVER_URL:MONGO_PORT/?authSource=admin
+let mongoURI = "";
+
+if (PROGRAM_MODE === "local") {
+    mongoURI = `mongodb://127.0.0.1:27017/${dbName}`;
+} else {
+    mongoURI = `mongodb://${MONGO_USER}:${MONGODB_PASSWORD}@${MONGODB_URI}:${MONGODB_PORT}/${dbName}?authSource=admin`;
+    // mongodb://USERNASME:PASSWORD@MONGO_SERVER_URL:MONGO_PORT/?authSource=admin
+}
+
+
 const connectDB = async (): Promise<void> => {
     try {
         await mongoose.connect(mongoURI, {
-           // useNewUrlParser: true,
+            // useNewUrlParser: true,
             //useUnifiedTopology: true, // Add this line for modern versions
         });
 
