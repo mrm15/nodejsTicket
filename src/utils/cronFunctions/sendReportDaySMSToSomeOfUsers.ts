7b2b1aa8060f7ef8bdb7d96bf59369dc253e3a3b
mrm15 <mrm.learn@gmail.com>
2024-08-02 00:25:08 +0330

    Update sendReportDaySMSToSomeOfUsers.ts

--- src/utils/cronFunctions/sendReportDaySMSToSomeOfUsers.ts
+++ src/utils/cronFunctions/sendReportDaySMSToSomeOfUsers.ts
@@ -1,41 +1,10 @@
 import {calculateTodayReport} from "../calculateTodayReport";
 import {sendSMSBoreshPlaxiShab} from "../../SMS/SMS.IR/sendSms";
 import {logEvents} from "../logEvents";
+import {sendSMSTodayReport} from "../sendSMSTodayReport";
 
 export const sendReportDaySMSToSomeOfUsers = async () => {
-    const sendSMSTodayReport = async (mobile: string, name: string, calculateTodayReportResult: any) => {
 
-        const {
-            plaksi2_8Value,
-            simplePunchValue,
-            proPunchValue,
-            doubleValue,
-            duqi10milValue,
-            duqi5milValue,
-            ESTILFELEZ,
-            CHALANDSUEDI,
-            NEONPLASTIC,
-            NEONFELAXI,
-        } = calculateTodayReportResult
-        const resultOfSendSMS = await sendSMSBoreshPlaxiShab(
-            {
-                mobile: mobile,
-                ADMINNAME: name,
-                plaksi2_8Value,
-                simplePunchValue,
-                proPunchValue,
-                doubleValue,
-                duqi10milValue,
-                duqi5milValue,
-                ESTILFELEZ,
-                CHALANDSUEDI,
-                NEONPLASTIC,
-                NEONFELAXI,
-            }
-        )
-
-        return resultOfSendSMS
-    }
 
     const calculateTodayReportResult = await calculateTodayReport();
     const res1 = await sendSMSTodayReport("09126970541", "جواد ", calculateTodayReportResult)
