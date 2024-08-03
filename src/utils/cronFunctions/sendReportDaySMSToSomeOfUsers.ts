9d6e7481f9bb19d6e3f339a0454db0ca7df92ff8
mrm15 <mrm.learn@gmail.com>
2024-08-03 16:59:09 +0330

    sendReportDaySMSToSomeOfUsers.ts

--- src/utils/cronFunctions/sendReportDaySMSToSomeOfUsers.ts
+++ src/utils/cronFunctions/sendReportDaySMSToSomeOfUsers.ts
@@ -1,16 +1,54 @@
 import {calculateTodayReport} from "../calculateTodayReport";
-import {sendSMSBoreshPlaxiShab} from "../../SMS/SMS.IR/sendSms";
+import {
+    sendSMSAdminChaleniumSuedi,
+    sendSMSAdminEstilFelez, sendSMSAdminLaserDouble,
+    sendSMSAdminNeon,
+    sendSMSAdminPlastic,
+    sendSMSAdminSMD,
+    sendSMSBoreshPlaxiShab
+} from "../../SMS/SMS.IR/sendSms";
 import {logEvents} from "../logEvents";
-import {sendSMSTodayReport} from "../sendSMSTodayReport";
 
 export const sendReportDaySMSToSomeOfUsers = async () => {
 
 
     const calculateTodayReportResult = await calculateTodayReport();
-    const res1 = await sendSMSTodayReport("09126970541", "جواد ", calculateTodayReportResult)
-    const res2 = await sendSMSTodayReport("09384642159", "محمد ", calculateTodayReportResult)
-    const re4 = await sendSMSTodayReport("09126544833", "ایمان  ", calculateTodayReportResult)
-    // const re5 = await sendSMSTodayReport("09126544833", "ایمان  ", calculateTodayReportResult)
+
+    // ارسال پیامک به جواد سرایی
+    const res1 = await sendSMSBoreshPlaxiShab({ ...calculateTodayReportResult, mobile: "09126970541" , ADMINNAME:"جواد ", })
+
+    // ارسال پیامک به ایمان که فقط استیل و فلز پیامک میشه طبق قالب خودش
+    const re4 = await sendSMSAdminEstilFelez({ ...calculateTodayReportResult, mobile: "09126544833" , ADMINNAME:"ایمان  ", })
+
+
+    // ارسال پیامک به  محمد میرعرب
+    const res2 = await sendSMSBoreshPlaxiShab({ ...calculateTodayReportResult, mobile: "09384642159" , ADMINNAME:"محمد ", })
+
+    // ارسال پیامک به  علی پسرخاله
+    const res555 = await sendSMSBoreshPlaxiShab({ ...calculateTodayReportResult, mobile: "09300220117" , ADMINNAME:"علی پسرخاله  ", })
+
+    const res66 = await sendSMSBoreshPlaxiShab({ ...calculateTodayReportResult, mobile: "09908425653" , ADMINNAME:"زهرای   ", })
+
+
+    // // ارسال پیامک به رضا سرایی فقط  چلنیوم و سوئدی ارسال میشه توی قالب خودش
+    // const res31 = await sendSMSAdminChaleniumSuedi({ ...calculateTodayReportResult, mobile: "09125662506" , ADMINNAME:"رضا سرایی  ", })
+    //
+    //
+    //
+    // const re99 = await sendSMSAdminLaserDouble({ ...calculateTodayReportResult, mobile: "09126544833" , ADMINNAME:"ایمان  ", })
+    //
+    // // ارسال پیامک به محمد شمس که فقط تعداد نور بهش پیامک میشه
+    // const res5 = await sendSMSAdminSMD({ ...calculateTodayReportResult, mobile: "09369576409" , ADMINNAME:"محمد شمس  ", })
+    //
+    //
+    // // ارسال پیامک به علی رجنی بخش پلاستیک که فقط طبق قالب خودش فقط آمار پلاستیک پیامک میشه
+    // const res6 = await sendSMSAdminPlastic({ ...calculateTodayReportResult, mobile: "09304774849" , ADMINNAME:"علی رجنی  ", })
+    //
+    // // ارسال پیامک به مهدی افتاده بخش نئون که فقط طبق قالب خودش فقط آمار نئون پیامک میشه
+    // const res7 = await sendSMSAdminNeon({ ...calculateTodayReportResult, mobile: "09304774849" , ADMINNAME:"مهدی افتاده  ", })
+
+
+
 
 
     const message = `پیامک گزارش برای مخاطبین ارسال شد
