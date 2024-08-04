1d762aa8c85f526c7344c6d56c743383054fe07b
mrm15 <mrm.learn@gmail.com>
2024-08-04 12:25:14 +0330

    Update getTodayReportSms.ts

--- src/controllers/reportsController/getTodayReportSms.ts
+++ src/controllers/reportsController/getTodayReportSms.ts
@@ -1,7 +1,14 @@
 import {NextFunction, Response} from 'express';
 import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
 import {calculateTodayReport} from "../../utils/calculateTodayReport";
-import {sendSMSBoreshPlaxiShab} from "../../SMS/SMS.IR/sendSms";
+import {
+    sendSMSAdminChaleniumSuedi,
+    sendSMSAdminLaserDouble, sendSMSAdminNeon, sendSMSAdminPlastic,
+    sendSMSAdminSMD,
+    sendSMSBoreshPlaxiShab
+} from "../../SMS/SMS.IR/sendSms";
+import {destinationPhoneNumberArray} from "../../utils/cronFunctions/destinationPhoneNumber";
+import {logEvents} from "../../utils/logEvents";
 
 
 const getTodayReportSms = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
@@ -15,34 +22,27 @@ const getTodayReportSms = async (req: CustomRequestMyTokenInJwt, res: Response, 
     }
 
 
+    const calculateTodayReportResult = await calculateTodayReport();
+
     try {
 
-        const calculateTodayReportResult = await calculateTodayReport();
-        // const resultOfSendSMS = await sendSMSTodayReport("09300220117","علی ",calculateTodayReportResult)
-        // const resultOfSendSMS1 = await sendSMSTodayReport("09908425653","زهرای ",calculateTodayReportResult)
-        const resultAli  = await sendSMSBoreshPlaxiShab({
-            ...calculateTodayReportResult,
-            mobile:"09384642159",
-            ADMINNAME:"محمد",
-        })
 
-        // // ارسال پیامک به رضا سرایی فقط  چلنیوم و سوئدی ارسال میشه توی قالب خودش
-        // const res31 = await sendSMSAdminChaleniumSuedi({ ...calculateTodayReportResult, mobile: "09125662506" , ADMINNAME:"رضا سرایی  ", })
-        // const re99 = await sendSMSAdminLaserDouble({ ...calculateTodayReportResult, mobile: "09126544833" , ADMINNAME:"ایمان  ", })
-        // // ارسال پیامک به محمد شمس که فقط تعداد نور بهش پیامک میشه
-        // const res5 = await sendSMSAdminSMD({ ...calculateTodayReportResult, mobile: "09369576409" , ADMINNAME:"محمد شمس  ", })
-        // // ارسال پیامک به علی رجنی بخش پلاستیک که فقط طبق قالب خودش فقط آمار پلاستیک پیامک میشه
-        // const res6 = await sendSMSAdminPlastic({ ...calculateTodayReportResult, mobile: "09304774849" , ADMINNAME:"علی رجنی  ", })
-        // // ارسال پیامک به مهدی افتاده بخش نئون که فقط طبق قالب خودش فقط آمار نئون پیامک میشه
-        // const res7 = await sendSMSAdminNeon({ ...calculateTodayReportResult, mobile: "09304774849" , ADMINNAME:"مهدی افتاده  ", })
+        debugger
+        const smsPromises = destinationPhoneNumberArray.map(({name, phoneNumber, renderFunction}) => {
+            return renderFunction({...calculateTodayReportResult, mobile: phoneNumber, ADMINNAME: name});
+        });
+
+        const results = await Promise.all(smsPromises);
+
+        const message = `پیامک گزارش برای مخاطبین ارسال شد
+            ${destinationPhoneNumberArray.map(({phoneNumber}) => phoneNumber).join(',\n')}
+        `;
+        await logEvents(message, "smsReportEveryDay.txt");
+
+        // console.log("All SMS sent successfully:", results);
 
 
-
-
-
-
-
-        if (resultAli) {
+        if (!!results) {
             res.status(200).json({
                 calculateTodayReportResult,
                 message: 'تسک انجام شد.',
