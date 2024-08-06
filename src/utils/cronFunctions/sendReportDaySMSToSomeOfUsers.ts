18a08baf6669cc742cf0998f0ca9c34b5b8e9855
mrm15 <mrm.learn@gmail.com>
2024-08-06 23:39:11 +0330

    Update sendReportDaySMSToSomeOfUsers.ts

--- src/utils/cronFunctions/sendReportDaySMSToSomeOfUsers.ts
+++ src/utils/cronFunctions/sendReportDaySMSToSomeOfUsers.ts
@@ -5,7 +5,7 @@ import {
     sendSMSAdminNeon,
     sendSMSAdminPlastic,
     sendSMSAdminSMD,
-    sendSMSBoreshPlaxiShab
+    sendSMSBoreshPlaxiShab, SendSmsResponse
 } from "../../SMS/SMS.IR/sendSms";
 import {logEvents} from "../logEvents";
 import {destinationPhoneNumberArray} from "./destinationPhoneNumber";
@@ -13,22 +13,22 @@ import {destinationPhoneNumberArray} from "./destinationPhoneNumber";
 
 
 export const sendReportDaySMSToSomeOfUsers = async () => {
-
-
+    debugger
     try {
         const calculateTodayReportResult = await calculateTodayReport();
 
-        const smsPromises = destinationPhoneNumberArray.map(({ name, phoneNumber, renderFunction }) => {
-            return renderFunction({ ...calculateTodayReportResult, mobile: phoneNumber, ADMINNAME: name });
-        });
-
-        const results = await Promise.all(smsPromises);
+        debugger
+        const results = await Promise.all(destinationPhoneNumberArray.map(async ({ name, phoneNumber, renderFunction }) => {
+            debugger
+             const responseSMS = await  renderFunction({ ...calculateTodayReportResult, mobile: phoneNumber, ADMINNAME: name });
+            console.log(`sms ${name}  sended`)
+            return responseSMS
+        }));
 
         const message = `پیامک گزارش برای مخاطبین ارسال شد
             ${destinationPhoneNumberArray.map(({ phoneNumber }) => phoneNumber).join(',\n')}
         `;
         await logEvents(message, "smsReportEveryDay.txt");
-
         console.log("All SMS sent successfully:", results);
     } catch (error) {
         console.error("Error sending SMS:", error);
