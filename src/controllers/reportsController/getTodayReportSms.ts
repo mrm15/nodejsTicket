79263712876a013c852616db0953ded5f6d6d531
mrm15 <mrm.learn@gmail.com>
2024-08-06 23:37:20 +0330

    Update getTodayReportSms.ts

--- src/controllers/reportsController/getTodayReportSms.ts
+++ src/controllers/reportsController/getTodayReportSms.ts
@@ -9,6 +9,7 @@ import {
 } from "../../SMS/SMS.IR/sendSms";
 import {destinationPhoneNumberArray} from "../../utils/cronFunctions/destinationPhoneNumber";
 import {logEvents} from "../../utils/logEvents";
+import {sendReportDaySMSToSomeOfUsers} from "../../utils/cronFunctions/sendReportDaySMSToSomeOfUsers";
 
 
 const getTodayReportSms = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
@@ -22,36 +23,38 @@ const getTodayReportSms = async (req: CustomRequestMyTokenInJwt, res: Response, 
     }
 
 
-    const calculateTodayReportResult = await calculateTodayReport();
+    // const calculateTodayReportResult = await calculateTodayReport();
 
     try {
+          await sendReportDaySMSToSomeOfUsers()
 
 
-        debugger
-        const smsPromises = destinationPhoneNumberArray.map(({name, phoneNumber, renderFunction}) => {
-            return renderFunction({...calculateTodayReportResult, mobile: phoneNumber, ADMINNAME: name});
-        });
-
-        const results = await Promise.all(smsPromises);
-
-        const message = `پیامک گزارش برای مخاطبین ارسال شد
-            ${destinationPhoneNumberArray.map(({phoneNumber}) => phoneNumber).join(',\n')}
-        `;
-        await logEvents(message, "smsReportEveryDay.txt");
+        // debugger
+        // const smsPromises = destinationPhoneNumberArray.map(({name, phoneNumber, renderFunction}) => {
+        //     return renderFunction({...calculateTodayReportResult, mobile: phoneNumber, ADMINNAME: name});
+        // });
+        //
+        // const results = await Promise.all(smsPromises);
+        //
+        // const message = `پیامک گزارش برای مخاطبین ارسال شد
+        //     ${destinationPhoneNumberArray.map(({phoneNumber}) => phoneNumber).join(',\n')}
+        // `;
+        // await logEvents(message, "smsReportEveryDay.txt");
 
         // console.log("All SMS sent successfully:", results);
 
 
-        if (!!results) {
+        if (true) {
             res.status(200).json({
-                calculateTodayReportResult,
+                // calculateTodayReportResult,
                 message: 'تسک انجام شد.',
             })
             return;
-        } else {
-            res.status(500).json({message: "پیام ارسال نشد!!!"});
-            return
         }
+        // else {
+        //     res.status(500).json({message: "پیام ارسال نشد!!!"});
+        //     return
+        // }
         /////////////////////////////////////////
 
     } catch (error: any) {
