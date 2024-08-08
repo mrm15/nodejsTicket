8f8f3a133806f820f008a6491f382232f9340b8d
mrm15 <mrm.learn@gmail.com>
2024-08-09 02:25:41 +0330

    Create getDataCollection.ts

---
+++ src/controllers/utility/collectionsHandlers/getDataCollection.ts
@@ -0,0 +1,24 @@
+import {buildFilterObject} from "./filterUtils";
+import {fetchPaginatedResults, SortOptions} from "./queryUtils";
+import {User} from "../../../models/User";
+import {Model} from "mongoose";
+
+export const getDataCollection =async (bodyData:any, collectionName:Model<any>)=>{
+    const {page = 1, pageSize = 2, filters = []} = bodyData;
+
+    // Build the filter object
+    const filterObject = buildFilterObject(filters);
+
+    // Define sorting options
+    const sortOptions: SortOptions = {createdAt: 1};
+
+    // Define pagination options
+    const paginationOptions = {page, pageSize};
+    debugger        // Fetch the users with pagination
+    return await fetchPaginatedResults(
+        collectionName,
+        filterObject,
+        paginationOptions,
+        sortOptions,
+    )
+}
