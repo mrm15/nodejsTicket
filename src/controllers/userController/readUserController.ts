25ac090bb6628e75287f22ced8389bdbf24d969e
mrm15 <mrm.learn@gmail.com>
2024-08-09 02:25:33 +0330

    Update readUserController.ts

--- src/controllers/userController/readUserController.ts
+++ src/controllers/userController/readUserController.ts
@@ -1,10 +1,11 @@
 import {Request, Response, NextFunction} from 'express';
-import {IUser, User} from "../../models/User";
 import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
 import {ACCESS_LIST} from "../../utils/ACCESS_LIST";
 import {checkAccessList} from "../../utils/checkAccessList";
+import {getDataCollection} from "../utility/collectionsHandlers/getDataCollection";
 import {buildFilterObject} from "../utility/collectionsHandlers/filterUtils";
 import {fetchPaginatedResults, SortOptions} from "../utility/collectionsHandlers/queryUtils";
+import {User} from "../../models/User";
 
 const readUserController = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
     const {myToken} = req;
@@ -33,41 +34,12 @@ const readUserController = async (req: CustomRequestMyTokenInJwt, res: Response,
         // Define pagination options
         const paginationOptions = {page, pageSize};
         debugger        // Fetch the users with pagination
-        const myFetchedData  = await fetchPaginatedResults(
-            User,
-            filterObject,
-            paginationOptions,
-            sortOptions,
-        )
-
-        const columnDefs = [
-            {minWidth: 150, headerName: "name", field: "name"},
-            {minWidth: 150, headerName: "شماره تماس", field: "phoneNumber"},
-            {minWidth: 150, headerName: "phoneNumber3", field: "phoneNumber3"},
-            {minWidth: 150, headerName: "postalCode", field: "postalCode"},
-            {minWidth: 150, headerName: "profilePictureUrl", field: "profilePictureUrl"},
-            {minWidth: 150, headerName: "province", field: "province"},
-            {minWidth: 150, headerName: "registerNumberCompany", field: "registerNumberCompany"},
-            {minWidth: 150, headerName: "tasks", field: "tasks"},
-            {minWidth: 150, headerName: "tickets", field: "tickets"},
-            {minWidth: 150, headerName: "title", field: "title"},
-            {minWidth: 150, headerName: "tokens", field: "tokens"},
-            {minWidth: 150, headerName: "updateAt", field: "updateAt"},
-            {minWidth: 150, headerName: "userName", field: "userName"},
-            {minWidth: 150, headerName: "website", field: "website"}
-        ];
-
-        const rowData = [...myFetchedData.results];
-        // rowData = rowData.map(r=>r.phoneNumber)
-
-        const list = {columnDefs, rowData};
-
+        const myFetchedData = await getDataCollection(req.body, User)
         res.status(200).json({
 
             ...myFetchedData
         });
         return;
-
     } catch (error) {
         res.status(500).json({error});
         return;
