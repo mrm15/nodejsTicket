3191056f9c56bb6e8f6fddc68d7aab2cdb2d8411
mrm15 <mrm.learn@gmail.com>
2024-08-11 01:36:04 +0330

    Update calculateTodayReport.ts

--- src/utils/calculateTodayReport.ts
+++ src/utils/calculateTodayReport.ts
@@ -2,7 +2,7 @@ import {calculatePivot, formatDateForBackend, reportArray} from "./functions";
 import {getHeaderAndRowsDetails, hesabfaApiRequest} from "../controllers/utility/hesabfa/functions";
 import {getCurrentTimeStamp} from "./timing";
 
-export const calculateTodayReport = async () => {
+export const calculateTodayReport = async (resultType: "all" | "detail" = "all") => {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
@@ -35,35 +35,16 @@ export const calculateTodayReport = async () => {
         throw new Error("مشکل در دریافت اطلاعات از حسابفا")
     }
 
-    debugger
+
     let temp11: any = (getHeaderAndRowsDetails(myResult.response?.data.Result.List))
     temp11 = temp11.rows;
     temp11 = temp11.filter((row: any) => row.myStatus === 1)
     const currentTimeHere = getCurrentTimeStamp()?.toLocaleTimeString('fa-ir')
     /////////////////////////////////////////
-    let smsText = ""
 
-    // reportArray.forEach(row => {
-    //     const resultOfCalculatePivot: [] = calculatePivot(
-    //         {
-    //             filterTextForPivot: row.filterTextForPivot,
-    //             totalData: temp11,
-    //             myKey: row.myKey,
-    //             sumKey: row.sumKey,
-    //             countKey: row.countKey,
-    //         }
-    //     );
-    //     console.log(row.caption)
-    //     console.log(resultOfCalculatePivot)
-    //     const totalQuantity = resultOfCalculatePivot.reduce((acc: number, rowReduce: any) => {
-    //         return acc + rowReduce[row.countKey]
-    //     }, 0)
-    //     smsText += " "+ row.caption + ": " + totalQuantity?.toFixed(2) + "  ";
-    // })
-
-    const countKeyHere = reportArray[1].countKey;
 
     const getPivotValue = (index: any) => {
+
         const pivotResult = calculatePivot({
             filterTextForPivot: reportArray[index].filterTextForPivot,
             totalData: temp11,
@@ -73,39 +54,72 @@ export const calculateTodayReport = async () => {
             giveMeTotalSum: true
         });
         return (pivotResult?.toFixed(2)) || 0;
-    };
+    }
 
-    const plaksi2_8Value = getPivotValue(0);
-    const simplePunchValue = getPivotValue(1);
-    const proPunchValue = getPivotValue(2);
-    const doubleValue = getPivotValue(3);
-    const duqi10milValue = getPivotValue(4);
-    const duqi5milValue = getPivotValue(5);
 
-    const ESTILFELEZ = getPivotValue(6);
-    const CHALANDSUEDI = getPivotValue(7);
-    const NEONPLASTIC = getPivotValue(8);
-    const NEONFELAXI = getPivotValue(9);
-    const SMD = getPivotValue(10);
-    const STICKER_OJRAT = getPivotValue(11);
-    const AMAR_VARAQ = getPivotValue(12);
-    const AMAR_PVC = getPivotValue(13);
+    if (resultType === "all") {
+        const plaksi2_8Value = getPivotValue(0);
+        const simplePunchValue = getPivotValue(1);
+        const proPunchValue = getPivotValue(2);
+        const doubleValue = getPivotValue(3);
+        const duqi10milValue = getPivotValue(4);
+        const duqi5milValue = getPivotValue(5);
 
-    return {
-        plaksi2_8Value,
-        simplePunchValue,
-        proPunchValue,
-        doubleValue,
-        duqi10milValue,
-        duqi5milValue,
-        ESTILFELEZ,
-        CHALANDSUEDI,
-        NEONPLASTIC,
-        NEONFELAXI,
-        SMD,
-        STICKER_OJRAT,
-        AMAR_VARAQ,
-        AMAR_PVC,
+        const ESTILFELEZ = getPivotValue(6);
+        const CHALANDSUEDI = getPivotValue(7);
+        const NEONPLASTIC = getPivotValue(8);
+        const NEONFELAXI = getPivotValue(9);
+        const SMD = getPivotValue(10);
+        const STICKER_OJRAT = getPivotValue(11);
+        const AMAR_VARAQ_Estil = getPivotValue(12);
+        const AMAR_PVC = getPivotValue(13);
+        const PLAKSI_Varaq = getPivotValue(14);
+
+        return {
+            plaksi2_8Value,
+            simplePunchValue,
+            proPunchValue,
+            doubleValue,
+            duqi10milValue,
+            duqi5milValue,
+            ESTILFELEZ,
+            CHALANDSUEDI,
+            NEONPLASTIC,
+            NEONFELAXI,
+            SMD,
+            STICKER_OJRAT,
+            AMAR_VARAQ_Estil,
+            AMAR_PVC,
+            PLAKSI_Varaq
+        }
+    } else if (resultType === "detail") {
+
+        const resData = reportArray.map(row => {
+
+            const caption= row.caption
+            const temp =  calculatePivot({
+                filterTextForPivot: row.filterTextForPivot,
+                totalData: temp11,
+                myKey: row.myKey,
+                sumKey: row.sumKey,
+                countKey: row.countKey,
+                giveMeTotalSum : false
+
+
+            })
+
+
+
+            return {
+                caption,
+                temp,
+            }
+
+        })
+
+        return resData
+
+
     }
 
 }
