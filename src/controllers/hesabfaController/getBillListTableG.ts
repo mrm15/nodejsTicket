4f38a8733784651f5ca25e98c749b628cb48cad1
mrm15 <mrm.learn@gmail.com>
2024-08-31 15:36:42 +0330

    change queryData getBillListTableG.ts

--- src/controllers/hesabfaController/getBillListTableG.ts
+++ src/controllers/hesabfaController/getBillListTableG.ts
@@ -11,43 +11,64 @@ const getBillListTableG = async (req: CustomRequestMyTokenInJwt, res: Response, 
             return res.status(200).json({message: 'Token not found in the request'});
         }
 
-        const arrayListToCheck = [ACCESS_LIST.ersal , ACCESS_LIST.basteBandi]
+        const arrayListToCheck = [ACCESS_LIST.ersal, ACCESS_LIST.basteBandi]
         const hasAccessTo = await checkAccessList({phoneNumber: myToken.phoneNumber, arrayListToCheck})
         if (!hasAccessTo) {
             res.status(403).json({message: 'شما مجوز دسترسی به این بخش را ندارید.'});
             return
         }
 
-        const {page = 1, pageSize = 2, filters = []} = req.body;
+        const {page = 1, pageSize = 3, filters = []} = req.body;
 
         const skip = (page - 1) * pageSize;
         const ttt = {
-            page , pageSize,
-            type: 0, // فقط فاکتور فروش
-            queryInfo: {
-                SortDesc: true,
-                Take: pageSize,
-                skip,
-                filter: [
+            type: 0,
+            "queryInfo": {
+                // "SortBy": 'Date',
+                "SortBy": "Id",
+                // "SortBy": "Number",
 
-                    {property: "Status", operator: "=", value: 1} , // فقط تایید شده ها رو میخوایم
-                    ...filters],
+                "SortDesc": true,
+                "Take": pageSize,
+                "Skip": skip,
+                "filters": [
+                    ...filters,
+                    {"property": "Status", "operator": "=", "value": 1},
+                ]
             }
         }
+        const ttt1 = {
+            type: 0, // فقط فاکتور فروش
+            queryInfo: {
+                SortBy: "Date",
+                // "SortBy": "Id",
+                // "SortBy": "Number",
+                SortDesc: true,
+                Take: pageSize,
+                Skip: skip,
+                filters: [
+
+                    {Property: "Status", Operator: "=", Value: 1}, // فقط تایید شده ها رو میخوایم
+                    // ...filters
+                ],
+
+            }
+        }
+        debugger
         const url = "invoice/getinvoices";
-        const myResult = await getDataCollectionFromHesabfa(ttt,url);
+        const myResult = await getDataCollectionFromHesabfa(ttt, url);
 
         const results = myResult.response?.data?.Result?.List
         const totalDocuments = myResult.response?.data?.Result?.TotalCount
-        const currentPage= page
-        res.status(200).json({results,totalDocuments,currentPage,pageSize});
+        const currentPage = page
+        res.status(200).json({results, totalDocuments, currentPage, pageSize});
         return;
 
-    } catch (error:any) {
+    } catch (error: any) {
 
-        debugger
-        if(error.hostname==='api.hesabfa.com'){
-            error.message="هیچ پاسخی از حسابفا دریافت نشد!";
+
+        if (error.hostname === 'api.hesabfa.com') {
+            error.message = "هیچ پاسخی از حسابفا دریافت نشد!";
         }
         res.status(500).json({error});
         return
