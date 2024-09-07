4bc717906992d8bae3e95910290a805203a3f1c2
mrm15 <mrm.learn@gmail.com>
2024-09-07 10:37:52 +0330

    optimize code  refreshTokenController.ts

--- src/controllers/refreshTokenController.ts
+++ src/controllers/refreshTokenController.ts
@@ -5,113 +5,86 @@ import {getUserInfoByPhoneNumber} from "./LoginRegisterSms/getUserInfoByPhoneNum
 import {generateAccessToken, generateRefreshToken} from "./LoginRegisterSms/generateAccessToken";
 import {getUserAgentData} from "./LoginRegisterSms/getUserAgentData";
 import {getCurrentTimeStamp} from "../utils/timing";
-import {getRoleAccessList} from "./LoginRegisterSms/getRoleAccessList"; // Adjust the path and import as per your project structure
+import {getRoleAccessList} from "./LoginRegisterSms/getRoleAccessList";
+import {clearJwtCookie, setJwtCookie} from "./utility/cookieHelpers/cookieHelpers";
 
 const handleRefreshToken = async (req: Request, res: Response): Promise<void> => {
+
     const cookies = req.cookies;
+
     if (!cookies?.jwt) {
         res.status(401).json({message: "handleRefreshToken Not Found User"});
         return
     }
-    const refreshToken = cookies.jwt;
-
-    let phoneNumber = '';
-
+    const refreshToken = cookies?.jwt;
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!, async (err: any, decoded: any) => {
         if (err) {
             res.status(403).json({error: err}); // Forbidden
             return
         }
+        let phoneNumber = '';
         phoneNumber = decoded.phoneNumber
-    });
-
-
-    res.clearCookie('jwt', {httpOnly: true, sameSite: 'none', secure: true});
-
-
-    const foundUser: IUser | null = await User.findOne({phoneNumber}).exec();
-
-
-    // Detected refresh token reuse!
-    const callbackIfUserHacked = async (err: any, decoded: any) => {
-        if (err) {
-            res.status(403).json({error: err}); // Forbidden
+        const foundUser: IUser | null = await User.findOne({phoneNumber}).exec();
+        // clearJwtCookie(res)
+        if (!foundUser) {
+            res.status(403).json({error: 'کاربری با مشخصات موجود در  توکن یافت نشد'})
             return
         }
 
-        const hackedUser = await User.findOne({phoneNumber: decoded?.phoneNumber}).exec();
-        if (hackedUser) {
-            hackedUser.tokens = [];
-            const result = await hackedUser.save();
 
-        }
-    }
-
-    if (!foundUser) {
-        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!, callbackIfUserHacked);
-        res.status(403).json({error: 'کاربری با مشخصات موجود در  توکن یافت نشد'})
-        return
-    }
-
-
-    const newTokensArray = foundUser.tokens.filter(rt => rt.refreshToken !== refreshToken);
-
-    // evaluate jwt
-
-    const callBack = async (err: any, decoded: any) => {
-        if (err) {
-
-            foundUser.tokens = [...newTokensArray];
-            const result = await foundUser.save();
-
-            res.sendStatus(403);
-            return
-        }
-
-        // Refresh token was still valid
-
-
-        const refreshToken = await generateRefreshToken(foundUser.phoneNumber)
-
-
+        const newTokensArray = foundUser.tokens.filter(rt => rt.refreshToken !== refreshToken);
+        const refreshToken1 = await generateRefreshToken(foundUser.phoneNumber)
         const {os, ip, useragent, loginTime} = getUserAgentData(req)
-
-        const newToken = {refreshToken, os, ip, useragent, loginTime}
-
-        // Saving refreshToken with current user
-        foundUser.tokens = [...newTokensArray, newToken];
-        //await foundUser.save();
+        const newToken = {refreshToken:refreshToken1, os, ip, useragent, loginTime}
+        foundUser.tokens = [...newTokensArray, newToken]
         try {
-            const result = await foundUser.save();
+            const result = await foundUser.save()
+            setJwtCookie(res, refreshToken)
+            const accessToken = await generateAccessToken(foundUser.phoneNumber)
+            const userInfo = await getUserInfoByPhoneNumber(phoneNumber)
+            res.status(200).json({userInfo, accessToken});
+            return;
 
-        } catch (error:any) {
+        } catch (error: any) {
             if (error.code === 11000) {
                 console.error('VersionError: Document version mismatch');
+                res.status(401).json({error: 'توکن کاربر ذخیره نشد'})
+
             } else {
                 console.error('Error saving document:', error);
+                res.status(401).json({error: 'توکن کاربر ذخیره نشد'})
             }
         }
 
+    })
+    return
+
+    // ======================================================
+    // jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!, async (err: any, decoded: any) => {
+    //     if (err) {
+    //         res.status(403).json({error: err}); // Forbidden
+    //         return
+    //     }
+    //     phoneNumber = decoded.phoneNumber;
+    //
+    // });
 
 
-        // Creates Secure Cookie with refresh token
-        res.cookie('jwt', refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'none',
-            maxAge: 24 * 60 * 60 * 1000
-        });
+    // res.clearCookie('jwt', {httpOnly: true, sameSite: 'none', secure: true});
+    // clearJwtCookie(res)
 
-        const accessToken = await generateAccessToken(foundUser.phoneNumber)
-        const userInfo = await getUserInfoByPhoneNumber(phoneNumber)
-        res.json({userInfo, accessToken});
-        return;
+    // const foundUser: IUser | null = await User.findOne({phoneNumber}).exec();
 
-    }
 
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!, callBack);
+    // Detected refresh token reuse!
 
+
+    // if (!foundUser) {
+    //     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!, callbackIfUserHacked);
+    //     res.status(403).json({error: 'کاربری با مشخصات موجود در  توکن یافت نشد'})
+    //     return
+    // }
 
 }
 
