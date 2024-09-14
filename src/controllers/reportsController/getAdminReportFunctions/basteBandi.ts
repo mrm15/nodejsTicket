b24339135e814b7732ff57a48f64132354488dd6
mrm15 <mrm.learn@gmail.com>
2024-09-14 12:08:22 +0330

    add calculate basteBandi.ts

---
+++ src/controllers/reportsController/getAdminReportFunctions/basteBandi.ts
@@ -0,0 +1,45 @@
+import {getBillsDataFromHesabfa} from "./getBillsDataFromHesabfa";
+import {timestampToTimeFromHesabfa} from "../../utility/timestampToTimeFromHesabfa";
+
+export const basteBandiCounter = async (myData: any) => {
+
+    const dateFiltered = myData.queryInfo.filters.find((row: any) => row.Property === "Date").Value;
+    const persianDate:string = timestampToTimeFromHesabfa(dateFiltered, true);
+
+    // توی  تگ تاریخ رو  با خط تیره ذخیره کردیم
+    const persianDateDash = persianDate.replaceAll("/" , "-")
+
+
+    debugger
+    const myData1 = {
+        type: 0, // Only sales invoices (type 0)
+        queryInfo: {
+            SortBy: 'Date',
+            SortDesc: true,
+            Take: 500,
+            Skip: 0,
+            filters: [
+                {Property: "Status", Operator: "=", Value: "1"}
+            ]
+        },
+    }
+
+
+    const billsDataFromHesabfa = await getBillsDataFromHesabfa(myData1);
+
+    const allData: any[] = billsDataFromHesabfa?.response?.data?.Result?.List
+    if (allData) {
+        const allFilteredData = allData.filter(row => row.Tag.includes(`db\":\"${persianDateDash}`))
+        const lengthOfData = allFilteredData.length
+        debugger
+        return {
+            title: "تعداد بسته بندی",
+            value: lengthOfData
+        }
+    }
+    return {
+        title: "تعداد بسته بندی",
+        value: 0
+    }
+
+}
