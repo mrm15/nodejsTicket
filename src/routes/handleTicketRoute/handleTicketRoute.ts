418c79fcc3375f68857e107e757ee40f25c7b28d
mrm15 <mrm.learn@gmail.com>
2024-09-25 11:25:29 +0330

    remove  readMyInboxTicketsController.ts

--- src/routes/handleTicketRoute/handleTicketRoute.ts
+++ src/routes/handleTicketRoute/handleTicketRoute.ts
@@ -7,19 +7,29 @@ import {ticketList} from "../../controllers/ticketController/ticketList";
 import {chatListTicketController} from "../../controllers/ticketController/chatListTicketController";
 import {ReadSentTicketController} from "../../controllers/ticketController/readSentTicketController";
 import {readMyAllTicketsController} from "../../controllers/ticketController/readMyAllTicketsController";
-import {readMyInboxTicketsController} from "../../controllers/ticketController/readMyInboxTicketsController";
 import {readDepartmentTicketsController} from "../../controllers/ticketController/readDepartmentTicketsController";
 
 const router = express.Router();
 
 router.post('/create', createTicketController);
 
+/**  get Ticket List   */
+router.post('/create', createTicketController);
+// خواندن کل تیکت های سیستم به صورت یکجا
 router.get('/read', readTicketController); // همه ی تیکت های سیستم رو یکجا میده
-router.get('/readSentTickets', ReadSentTicketController); // تیکت های ارسال من رو میره از توی کل تیکت ها میکشه بیرون و نشون میده
-router.get('/readMyAllTickets', readMyAllTicketsController)// done works correctly; // فقط تیکت هایی که در یوزر مدل قرار داره رو بفرست انجام شده
-router.get('/readMyInboxTickets', readMyInboxTicketsController);//done works correctly; //  مشترک بین دپارتمان و یورز که در یوزر مدل قرار داره
-router.get('/readDepartmentTickets', readDepartmentTicketsController); // تیکت های  دپارتمان که فقط ادمین دپارتمان بهشون دسترسی داره  tickets that is im my department  just admin of department has access to this part
+router.post('/read', readTicketController); // باصفحه بندی و فیلتر
 
+// خواندن تیکت هایی که خودم ارسال کردم. نگاه میکنیم ببینیم توی جدول تیکت ها  فرستنده اگه خودم بودم لیست بشه
+router.get('/readSentTickets', ReadSentTicketController); // تیکت های ارسال من رو میره از توی کل تیکت ها میکشه بیرون و نشون میده
+
+// باید برم توی جدول  تیکت هایی که به کاربر خاص ارجاع میشن ببینم  که منه کاربر چه تیکت هایی هست که به من ارجاع شده
+// و وضعیت خواندن هم براشون تعیین میکنم که  اگه نخونده بود  بولد بشه سمت فرانت
+router.get('/readMyAllTickets', readMyAllTicketsController)// done works correctly; // فقط تیکت هایی که در یوزر مدل قرار داره رو بفرست انجام شده
+
+// تیکت هایی که توی دپارتمان هستند و فقط ادمین دپارتمان میتونه ببینه
+// میرم از بین کل تیکت ها  اونایی که دپارتمان الانش  مثل دپارتمان خودمه رو میکشم بیرون و به کاربر نشون میدم.
+router.get('/readDepartmentTickets', readDepartmentTicketsController); // تیکت های  دپارتمان که فقط ادمین دپارتمان بهشون دسترسی داره  tickets that is im my department  just admin of department has access to this part
+/**  End Of   get Ticket List   */
 router.get('/chatList/:ticketId', chatListTicketController);
 router.post('/update', updateTicketController);
 router.delete('/delete/:id', deleteTicketController);
