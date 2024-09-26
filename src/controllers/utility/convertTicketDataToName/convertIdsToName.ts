6f121579f75d0d4741a7d1c4ed1e25c6209b10c3
mrm15 <mrm.learn@gmail.com>
2024-09-26 16:00:44 +0330

    Update convertIdsToName.ts

--- src/controllers/utility/convertTicketDataToName/convertIdsToName.ts
+++ src/controllers/utility/convertTicketDataToName/convertIdsToName.ts
@@ -1,6 +1,8 @@
 import {FlattenMaps} from "mongoose"
 import {IUser, User} from "../../../models/User";
 import {timestampToTime} from "../../../utils/timestampToTime";
+import {Department, IDepartment} from "../../../models/department";
+import {IStatus, Status} from "../../../models/status";
 // من هر چیزی رو که اینجا اضافه مینم بعدا توی میدلور فیلتر میرم و تغییر میدم و سرچ میکنم.
 //
 export const convertIdsToName = async (dt: {
@@ -17,10 +19,22 @@ export const convertIdsToName = async (dt: {
         dt.results = await Promise.all(ticketList.map(async (singleTicket) => {
             const row: any = {...singleTicket};
             const userFound: IUser = (await User.findOne({_id: row.userId}).lean())!;
+            const userFoundAssigned: IUser = (await User.findOne({_id: row.assignToUserId}).lean())!;
+            const departmentFound: IDepartment = (await Department.findOne({_id: row.assignedToDepartmentId}).lean())!;
+            const statusFound: IStatus = (await Status.findOne({_id: row.status}).lean())!;
+
+
+
             row['userCreateThisOrder'] = userFound?.name || '_'
             row['numberOfAttachments'] = row.attachments.length
             row['dateCreate'] = timestampToTime(row.createAt)
             row['lastChangeTime'] = timestampToTime(row.lastChangeTimeStamp)
+            row['statusText'] = statusFound?.name || "-" ;
+            row['assignedToDepartmentIdText'] = departmentFound?.name || "-" ;
+            row['assignToUserIdText'] = userFoundAssigned?.name || "-" ;
+            /////////////////////////////////////////////////////////////////////
+
+
             return row;
         }))
     } catch (error: any) {
