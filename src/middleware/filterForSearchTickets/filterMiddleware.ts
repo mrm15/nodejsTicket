d1196d3ad8b44eb0d18cd6ddc5dc284ed0d55af0
mrm15 <mrm.learn@gmail.com>
2024-09-28 14:12:31 +0330

    add customFieldHandlers.ts

--- src/middleware/filterForSearchTickets/filterMiddleware.ts
+++ src/middleware/filterForSearchTickets/filterMiddleware.ts
@@ -1,4 +1,6 @@
 // middleware/filterMiddleware.js
+import {customFieldHandlers} from "./customFieldHandlers";
+
 export const filterMiddleware = (req: { body: { filter: any; }; filter: any; }, res: any, next: () => void) => {
     let {filter} = req.body;
     // من به صورت دستی یه سری آیتم به جدول  نمایش لیست تیکت ها اضافه کردم که میخوام اینجا توی فیلتر اونا رو تغییر بدم تا وقتی میره توی فیلتر اصلی کار کنه
@@ -6,14 +8,15 @@ export const filterMiddleware = (req: { body: { filter: any; }; filter: any; }, 
     if (!filter) {
         return next();
     }
-    const has_IdFilter = filter.find((row: any) => row.property === "_id")
-    const hasTicketNumberFilter = filter.find((row: any) => row.property === "ticketNumber");
-
+    // فیلتر هایی که خودم دستی اضافه کردم اینا هستن #10001 رو توی پروژه سرچ کن
+    // Transform the filter using the custom field handlers
     filter = filter.map((row: any) => {
-        if (hasTicketNumberFilter && row.property === "ticketNumber") {
-            row.value = +row.value
+        // If the field is custom, execute the corresponding logic
+        if (customFieldHandlers[row.property]) {
+            customFieldHandlers[row.property](row);
         }
-    })
+        return row;
+    });
 
 
     req.body.filter = filter;
