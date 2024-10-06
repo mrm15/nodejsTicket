acaf0ff0969200848649fbadb8886d77b61b3984
mrm15 <mrm.learn@gmail.com>
2024-10-06 22:38:35 +0330

    Update addToAssignedTickets.ts

--- src/utils/forwardTicketUtils/addToAssignedTickets.ts
+++ src/utils/forwardTicketUtils/addToAssignedTickets.ts
@@ -1,4 +1,36 @@
-const addToAssignedTickets =async ()=>{
+import {ITicket, Ticket} from "../../models/ticket";
+import {ITicketAssignment, TicketAssignment} from "../../models/ticketAssignment ";
+import {Department} from "../../models/department";
 
+interface IInput {
+    ticketArray: ITicket[];
+    departmentId: string | null;
+    userId: string | null;
+    senderUserId:string;
+}
+
+const addToAssignedTickets = async ({ticketArray, departmentId, userId,senderUserId}: IInput) => {
+
+    const myList = await Promise.all(ticketArray.map(async (singleTicket: ITicket) => {
+        // آیدی اون تیکت رو میگیریم
+        const ticketFound: ITicket = (await Ticket.findOne({_id: singleTicket._id}).exec())!;
+        const newRow: any = {};
+        newRow.ticketId = ticketFound._id
+        newRow.assignedBy=senderUserId
+        newRow.createdAt = new Date();
+        // نگاه میکنیم به مقدار  کاربر  اگه ست شده بود به کاربر باید اختصاص بدیم و به دپارتمان توجه نکنیم
+        if (userId) {
+            newRow.assignmentType = "user"
+            newRow.assignedToDepartmentId = null
+            newRow.assignedToUserId = userId
+        } else if (departmentId && !userId) {
+            newRow.assignmentType = "department"
+            newRow.assignedToDepartmentId = departmentId
+            newRow.assignedToUserId = null
+        }
+        const result = await TicketAssignment.create(newRow)
+        return !!result;
+    }))
+    return myList.find(row => !row)
 }
 export default addToAssignedTickets
