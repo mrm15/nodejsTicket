35bc75255e1e0d654f30d9c657a9e122225e5545
mrm15 <mrm.learn@gmail.com>
2024-10-07 23:52:40 +0330

    Update readDepartmentTicketsController.ts

--- src/controllers/ticketController/readDepartmentTicketsController.ts
+++ src/controllers/ticketController/readDepartmentTicketsController.ts
@@ -4,9 +4,14 @@ import {ACCESS_LIST} from "../../utils/ACCESS_LIST";
 import {checkAccessList} from "../../utils/checkAccessList";
 import {userTicketTable} from "../../utils/userTicketTable";
 import {inboxTicketList} from "../../utils/inboxTicketList";
-import {Department} from "../../models/department";
+import {Department, IDepartment} from "../../models/department";
 import {departmentTickets} from "../../utils/getDepartmentTicketList";
 import mongoose from "mongoose";
+import getDepartmentByPhoneNumber from "../../utils/functions/getDepartmentByPhoneNumber";
+import {ITicketAssignment, TicketAssignment} from "../../models/ticketAssignment ";
+import {getDataCollection} from "../utility/collectionsHandlers/getDataCollection";
+import {ITicket, Ticket} from "../../models/ticket";
+import {convertIdsToName} from "../utility/convertTicketDataToName/convertIdsToName";
 
 const readDepartmentTicketsController = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
 
@@ -21,47 +26,37 @@ const readDepartmentTicketsController = async (req: CustomRequestMyTokenInJwt, r
 
     try {
 
-        // باید نگاه کنیم اگه طرف ادمین دپارتمان بود تیکت های دپارتمانش رو نشونش بدیم و دیگه نیازی به  چک کردن دسترسی ها نیست.
+        // اینجا میتونم چک کنم آیا این کاربر ادمین یک دپارتمان هست یا خیر؟
+        // بعدش میتونم چک کنم ادمین هر دپارتمانی هست همون دپارتمان تیکت هاشو از ارجاعات بگیرم بفرستم واسه فرانت
 
-        const { userId } = myToken.UserInfo.userData.userData;
-        const departmentList = await Department.find().lean();
-        const adminList = departmentList.map(singleDepartment => singleDepartment.managerUserId.toString());
+        // ولی اینجا فقط به دپارتمان کاربر نگاه میکنم و تیکت های اون دپارتمانی که کاربر درخواست داده بود میفرستم سمت فرانت
+        // توی فرانت این صفحه فقط در صورتی نمایش داده میشه که کاربر ادمین دپارتمان باشه.
+        const foundDepartment: IDepartment = await getDepartmentByPhoneNumber(myToken.phoneNumber)
+        const filters = req.body.filters || [];
+        filters.push({
+            property: 'assignedToDepartmentId',
+            value: foundDepartment._id,
+        });
+        filters.push({
+            property: 'isDeleteDestination',
+            value: false,
+        });
+        req.body.filters = filters;
 
-        debugger
-        // اگه کسی که این درخواست رو داده توی لیست ادمین های دپارتمان ها نبود پس بهش میگیم شرمنده تو اصلا اجاره نداری اینجا رو ببینی
-        if (!adminList.includes(userId)) {
-            res.status(403).json({message: 'شما مجوز دسترسی به این بخش را ندارید.'});
-            return
-        }
-        // گرفتن اون آیدی دپارتمانی که این کاربر ادمینش هست
-        let userDepartmentId: mongoose.Schema.Types.ObjectId | undefined;
-        departmentList.forEach(singleDepartment => {
-            if (singleDepartment.managerUserId?.toString() === userId) {
-                userDepartmentId = singleDepartment._id
+        // Fetch tickets
+        const myTicketAssignment = await getDataCollection(req.body, TicketAssignment)
+
+        let updatedTickets = myTicketAssignment
+
+        myTicketAssignment.results = await Promise.all(myTicketAssignment.results.map(async (singleAssignment: ITicketAssignment) => {
+            const ticketFound: ITicket = (await Ticket.findOne({_id: singleAssignment.ticketId}).lean())!;
+            return {
+                ...singleAssignment,
+                ...ticketFound
             }
-        })
-
-
-        if (userDepartmentId) {
-            // میتونستم از روی شماره تلفن یوزرآیدی رو بگیرم اما اینجا ترجیح دادم اینطوری کنم و از توی توکن بردارم.
-            const list = await departmentTickets({id: userDepartmentId})
-            res.status(200).json({
-                list, message: 'لیست بارگزاری شد.',
-            });
-            return;
-        } else {
-
-            const list = {
-                columnDefs: [], rowData: []
-
-            }
-            res.status(200).json({
-                list, message: 'لیست خالی است.',
-            });
-            return;
-        }
-
-
+        }))
+        updatedTickets = await convertIdsToName(myTicketAssignment)
+        return res.status(200).json(updatedTickets);
     } catch (error: any) {
 
         res.status(500).json({error: error.toString()});
