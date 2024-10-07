2dd945569aec3d89cba8d71f036dfcc0097eacce
mohammad mirarab <mrm.learn@gmail.com>
2024-10-07 11:51:39 +0330

    Update ticketAssignment .ts

--- src/models/ticketAssignment .ts
+++ src/models/ticketAssignment .ts
@@ -1,22 +1,34 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, {Document, Schema} from 'mongoose';
 
 // تعریف رابط (interface) سند TicketAssignment
 // این رابط نوع داده‌های ذخیره شده در مدل را مشخص می‌کند
 interface ITicketAssignment extends Document {
-    ticketId: mongoose.Schema.Types.ObjectId; // شناسه‌ی تیکت (از مجموعه‌ی Tickets)
-    assignedToUserId: mongoose.Schema.Types.ObjectId | null; // شناسه‌ی کاربری که تیکت به او اختصاص داده شده (اختیاری، اگر به دپارتمان تخصیص داده شود)
-    assignedToDepartmentId: mongoose.Schema.Types.ObjectId | null; // شناسه‌ی دپارتمانی که تیکت به آن تخصیص داده شده (اختیاری، اگر به کاربر تخصیص داده شود)
-    assignedBy: mongoose.Schema.Types.ObjectId; // شناسه‌ی ادمینی که تیکت را تخصیص داده
-    assignDate: Date; // تاریخ و زمان تخصیص تیکت
-    readDate: Date | null; // تاریخی که تیکت توسط کاربر خوانده شده (یا null اگر هنوز خوانده نشده)
-    readStatus: boolean; // وضعیت خوانده یا نخوانده بودن تیکت
-    numberOfAssign: number; // تعداد دفعاتی که این تیکت به کاربر تخصیص داده شده
-    assignmentType: 'user' | 'department'; // نوع تخصیص (به کاربر یا دپارتمان)
-    isDeletedForUser: boolean; // If user deleted it from their inbox
-    isDeletedForForwarder: boolean; // If forwarder deleted it from forwarded list
-
-    createdAt: Date; // تاریخ ایجاد رکورد تخصیص
-    updatedAt: Date; // تاریخ آخرین به‌روزرسانی رکورد
+    // شناسه‌ی تیکت (از مجموعه‌ی Tickets)
+    ticketId: mongoose.Schema.Types.ObjectId;
+    // شناسه‌ی کاربری که تیکت به او اختصاص داده شده (اختیاری، اگر به دپارتمان تخصیص داده شود)
+    assignedToUserId: mongoose.Schema.Types.ObjectId | null;
+    // شناسه‌ی دپارتمانی که تیکت به آن تخصیص داده شده (اختیاری، اگر به کاربر تخصیص داده شود)
+    assignedToDepartmentId: mongoose.Schema.Types.ObjectId | null;
+    //assignedToUserId or assignedToDepartmentId maybe delete from their inbox
+    isDeleteDestination: boolean;
+    // شناسه‌ی ادمینی که تیکت را تخصیص داده فرستنده تیکت اینجا قرار میگیره که میتونه یه کاربر عادی باشه یا ادمین یه دپارتمان که دسترسی ارجاع تیکت ها رو داره
+    assignedBy: mongoose.Schema.Types.ObjectId;
+    // isDeleteSender - If forwarder deleted it from forwarded list
+    isDeleteFromAssignedBy: boolean;
+    // تاریخ و زمان تخصیص تیکت
+    assignDate: Date;
+    // تاریخی که تیکت توسط کاربر خوانده شده (یا null اگر هنوز خوانده نشده)
+    readDate: Date | null;
+    // وضعیت خوانده یا نخوانده بودن تیکت
+    readStatus: boolean;
+    // تعداد دفعاتی که این تیکت به کاربر تخصیص داده شده - فعلا از این مورد استفاده ای نکردم!!
+    numberOfAssign: number;
+    // نوع تخصیص (به کاربر یا دپارتمان)
+    assignmentType: 'user' | 'department';
+    // تاریخ ایجاد رکورد تخصیص
+    createdAt: Date;
+    // تاریخ آخرین به‌روزرسانی رکورد
+    updatedAt: Date;
 }
 
 // تعریف اسکیمای (schema) TicketAssignment
@@ -37,10 +49,18 @@ const ticketAssignmentSchema: Schema<ITicketAssignment> = new mongoose.Schema({
         ref: 'Department', // اشاره به مجموعه‌ی Departments
         default: null, // اگر تیکت به دپارتمان تخصیص داده نشده باشد، مقدار null است
     },
+    isDeleteDestination: {
+        type: Boolean,
+        default: false, //
+    },
     assignedBy: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Admin', // اشاره به ادمینی که تیکت را تخصیص داده
         required: true, // این فیلد الزامی است
+    },
+    // is deleteFrom
+    isDeleteFromAssignedBy: {
+        type: Boolean,
+        default: false, //
     },
     assignDate: {
         type: Date,
@@ -77,13 +97,13 @@ const ticketAssignmentSchema: Schema<ITicketAssignment> = new mongoose.Schema({
 // ایندکس گذاری برای بهینه‌سازی کوئری‌ها
 // این ایندکس‌ها باعث می‌شوند که دسترسی به داده‌ها در این فیلدها سریع‌تر باشد
 
-ticketAssignmentSchema.index({ ticketId: 1 }); // ایندکس برای جستجو بر اساس شناسه تیکت
-ticketAssignmentSchema.index({ assignedToUserId: 1 }); // ایندکس برای جستجو بر اساس شناسه کاربر تخصیص داده شده
-ticketAssignmentSchema.index({ assignedToDepartmentId: 1 }); // ایندکس برای جستجو بر اساس شناسه دپارتمان تخصیص داده شده
-ticketAssignmentSchema.index({ readTicket: 1 }); // ایندکس برای جستجو بر اساس وضعیت خوانده شدن تیکت
-ticketAssignmentSchema.index({ assignDate: -1 }); // ایندکس برای مرتب‌سازی تیکت‌ها بر اساس تاریخ تخصیص (نزولی: جدیدترین تیکت‌ها اول نمایش داده می‌شوند)
+ticketAssignmentSchema.index({ticketId: 1}); // ایندکس برای جستجو بر اساس شناسه تیکت
+ticketAssignmentSchema.index({assignedToUserId: 1}); // ایندکس برای جستجو بر اساس شناسه کاربر تخصیص داده شده
+ticketAssignmentSchema.index({assignedToDepartmentId: 1}); // ایندکس برای جستجو بر اساس شناسه دپارتمان تخصیص داده شده
+ticketAssignmentSchema.index({readTicket: 1}); // ایندکس برای جستجو بر اساس وضعیت خوانده شدن تیکت
+ticketAssignmentSchema.index({assignDate: -1}); // ایندکس برای مرتب‌سازی تیکت‌ها بر اساس تاریخ تخصیص (نزولی: جدیدترین تیکت‌ها اول نمایش داده می‌شوند)
 
 // ایجاد و اکسپورت مدل TicketAssignment
 const TicketAssignment = mongoose.model<ITicketAssignment>('TicketAssignment', ticketAssignmentSchema);
 
-export { TicketAssignment, ITicketAssignment };
+export {TicketAssignment, ITicketAssignment};
