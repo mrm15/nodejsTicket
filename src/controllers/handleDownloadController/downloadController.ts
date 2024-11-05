8c94707257081f736f2053e19fe98f9012b62e0e
mrm15 <mrm.learn@gmail.com>
2024-11-06 03:09:02 +0330

    update  send header

--- src/controllers/handleDownloadController/downloadController.ts
+++ src/controllers/handleDownloadController/downloadController.ts
@@ -1,53 +1,36 @@
 import {Request, Response, NextFunction} from 'express';
-import {IUser, User} from "../../models/User";
-import {getCurrentTimeStamp} from "../../utils/timing";
 import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
-import {ACCESS_LIST} from "../../utils/ACCESS_LIST";
-import {checkAccessList} from "../../utils/checkAccessList";
-import {Department} from "../../models/department";
-import {stringToBoolean} from "../../utils/stringBoolean";
 import path from "path";
 import {getUserAgentData} from "../LoginRegisterSms/getUserAgentData";
 import {timestampToTime} from "../../utils/timestampToTime";
 
 const downloadController = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
-
-
     const {myToken} = req;
-
-
     const filePath = path.join(__dirname, '../../../uploads', req.params.id);
-    // Send the file
 
-
-    // res.status(500).send({filePath});
-    // return
-    // اینجا باید به دانلود فایل یکی اضافه کنم
-    // Send the file
     try {
-        res.sendFile(filePath, err => {
+        res.sendFile(filePath, (err) => {
             if (err) {
+                if (!res.headersSent) {
+                    const systemData = getUserAgentData(req);
+                    const {useragent, ...rest} = { ...systemData };
 
-                let systemData = getUserAgentData(req)
-                 let {useragent , ...rest} = {...systemData}
-                res.status(404).send({
-                    message: 'فایلی با این نام یافت نشد. تمامی مشخصات سیستم شما، آدرس آی پی و... جهت پیگیری ثبت شد.',
-                    ...rest,
-                    loginTime: timestampToTime(systemData.loginTime),
-                });
+                    res.status(404).send({
+                        message: 'فایلی با این نام یافت نشد. تمامی مشخصات سیستم شما، آدرس آی پی و... جهت پیگیری ثبت شد.',
+                        ...rest,
+                        loginTime: timestampToTime(systemData.loginTime),
+                    });
+                    return; // Exit early to ensure no further code is executed
+                }
             }
-        })
-        return
+        });
+        return; // Exit after initiating `sendFile`
     } catch (error) {
-        res.status(500).send({error: error?.toString()});
-        return
+        if (!res.headersSent) {
+            res.status(500).send({error: error?.toString()});
+            return; // Exit early after handling the error
+        }
     }
-
-
-    // res.status(200).json({message:filePath})
-    // return
-
-
 };
 
 export {downloadController};
