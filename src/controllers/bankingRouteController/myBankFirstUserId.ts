255298424560e9198a99fb454ee8c4df387eb20e
mohammad mirarab <mrm.learn@gmail.com>
2024-11-19 15:54:04 +0330

    update bank

--- src/controllers/bankingRouteController/myBankFirstUserId.ts
+++ src/controllers/bankingRouteController/myBankFirstUserId.ts
@@ -9,12 +9,13 @@ import {ACCESS_LIST} from "../../utils/ACCESS_LIST";
 import {checkAccessList} from "../../utils/checkAccessList";
 import {IRole, Role} from "../../models/roles";
 import {myPermissionsArray} from "../roleController/permissinsArray";
+import getBankingData from "../../utils/banking/getBankingData/getBankingData";
 
 
 const myBankFirstUserId = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
 
     const {myToken} = req;
-    const newRoleData = req.body;
+    const filters = req.body.filterItems;
 
     // res.status(201).json({myToken})
     //
@@ -29,76 +30,12 @@ const myBankFirstUserId = async (req: CustomRequestMyTokenInJwt, res: Response, 
     try {
 
 
-        const {phoneNumber} = myToken
 
 
-        const newRoleData = req.body
 
-        // آیا کاربر اجازه داره   کابری رو ثبت کنه؟
-        const arrayListToCheck = [
-            ACCESS_LIST.ROLES_CREATE
-        ]
-        const hasAccessToAddRole = await checkAccessList({phoneNumber, arrayListToCheck})
+        const result = await getBankingData({type:"user", filters, myToken })
 
-        if (!hasAccessToAddRole) {
-            res.status(403).json({message: 'شما مجوز دسترسی به این بخش را ندارید.'});
-            return
-        }
-
-        // check if this role name is uniq
-        const isThereAnyRoleWithThatName: IRole[] | null = await Role.findOne({name: newRoleData.name}).lean()
-
-        if (!!isThereAnyRoleWithThatName && isThereAnyRoleWithThatName?.length !== 0) {
-            res.status(409).json({
-                message: 'نام نقش تکراری',
-            });
-            return
-        }
-
-
-        // احتمال اینکه همون موقع یه نیروی ادمینی با ادمین اصلی لج کرده باشه ممکنه که اینجا بخواد مثلا مثلا نقش بیاد تعریف کنه که باید بگم شرمند
-        // البته کاربری توی توکن چک میشه
-        const userWantToAdd: { [key: string]: any; } = (await getUserInfoByPhoneNumber(myToken.phoneNumber))!;
-        if (!userWantToAdd) {
-            res.status(500).json({
-                message: 'کاربری  شما مورد تایید نیست!',
-            });
-            return
-        }
-
-
-        const userFound: IUser | null = await User.findOne({phoneNumber: myToken.phoneNumber})
-
-        if (!userFound) {
-            res.status(500).json({message: "کاربری تایید نشد."});
-            return
-        }
-
-        const userId = userFound?.id
-
-
-        const permissionsArray = [...myPermissionsArray]
-
-
-        const newRoleObject: any = {
-
-            name: newRoleData?.name || "بدون نام",
-            description: newRoleData?.description || " بدون توضیحات",
-            createAt: getCurrentTimeStamp(),
-            updateAt: getCurrentTimeStamp(),
-            userId,
-        }
-
-
-        permissionsArray.forEach(item => {
-            newRoleData.statusListCreate.forEach((frontItem: string) => {
-                newRoleObject[item] = (item === frontItem);
-            })
-        })
-
-
-        const result = await Role.create(newRoleObject);
-        res.status(200).json({result, message: ' اینم از نقش  جدید',});
+        res.status(200).json({result, message: '',});
         return;
 
     } catch (error) {
