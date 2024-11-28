001f66e5262389aac446fdebf038c91b69dd82cf
mrm15 <mrm.learn@gmail.com>
2024-11-28 03:38:02 +0330

    fixed sms

--- src/controllers/hesabfaController/sendAfterSavedBillSMS.ts
+++ src/controllers/hesabfaController/sendAfterSavedBillSMS.ts
@@ -7,59 +7,66 @@ import {timestampToTimeFromHesabfa} from "../utility/timestampToTimeFromHesabfa"
 import {p2e} from "../utility/NumericFunction";
 
 export const sendAfterSavedBillSMS = async (billData: any, adminSettings: IAdminSettings) => {
-    debugger
+        // Determine if the bill is verified
+        const isBillVerified = (billData.Status === "1" || billData.Status === 1);
 
-    // Determine if the bill is verified
-    const isBillVerified = (billData.Status === "1" || billData.Status === 1);
+        // Function to send SMS
+        const sendSMS = async (typeOfSendMessage: "submitBill" | "verifyBill") => {
+            const siteUrl = process.env.SITE_ADDRESS;
+            if (!siteUrl) {
+                throw new Error('Invalid SITE_ADDRESS: Environment variable SITE_ADDRESS is missing');
+            }
 
-    // Function to send SMS
-    const sendSMS = async (typeOfSendMessage: "submitBill" | "verifyBill") => {
-        const siteUrl = process.env.SITE_ADDRESS;
-        if (!siteUrl) {
-            throw new Error('Invalid SITE_ADDRESS: Environment variable SITE_ADDRESS is missing');
-        }
+            const itemLink = `${siteUrl}s/${billData.Number}`;
+            const destinationNumber = billData.Contact.Mobile;
+            const contactName = billData.ContactTitle;
+            const billLink = itemLink
+            // اینجا باید از توی تگ اطلاعات بیل نامبر رو بگیرم
+            let orderNumber = billData?.ticketNumber;
+            let sendSmsResult;
+            ////////////////////////////////////////
+            const orderName = billData.ContactTitle
+            const Sum = billData.Sum
+            const orderPrice = formatNumber(Sum)
 
-        const itemLink = `${siteUrl}s/${billData.Number}`;
-        const destinationNumber = billData.Contact.Mobile;
-        const contactName = billData.ContactTitle;
-        const billLink = itemLink
-        // اینجا باید از توی تگ اطلاعات بیل نامبر رو بگیرم
-        let orderNumber = billData?.ticketNumber;
-        let sendSmsResult;
-        ////////////////////////////////////////
-        const orderName =billData.ContactTitle
-        const Sum =billData.Sum
-        const orderPrice = formatNumber(Sum)
-
-        let billDate = timestampToTimeFromHesabfa(billData?.Date)?.split(",")[0]
-        billDate = p2e(billDate)
-        let the_user_name =billData.Contact.Name;
+            let billDate = timestampToTimeFromHesabfa(billData?.Date)?.split(",")[0]
+            billDate = p2e(billDate)
+            let the_user_name = billData.Contact.Name;
 
 
-        ////////////////////////////////////////
+            ////////////////////////////////////////
 
-        if (typeOfSendMessage === "submitBill") {
-            sendSmsResult = await sendSubmitBillSMS(
-                {mobile: destinationNumber, contactName, billLink}
-            )
+            if (typeOfSendMessage === "submitBill") {
+                sendSmsResult = await sendSubmitBillSMS(
+                    {mobile: destinationNumber, contactName, billLink}
+                )
+            } else {
+                debugger
+                sendSmsResult = await sendVerifyBillSMS(
+                    // {mobile: destinationNumber, contactName, billLink, orderNumber}
+                    {
+                        mobile: destinationNumber,
+                        contactName: the_user_name,
+                        orderName: orderName,
+                        orderPrice,
+                        DATE: billDate,
+                        orderNumber
+                    }
+                )
+            }
+
+            sendSmsResult.status
+        };
+
+        if (isBillVerified) {
+            if (adminSettings.sendSMSAfterVerifyBill) {
+                return await sendSMS("verifyBill");
+            }
         } else {
-            debugger
-            sendSmsResult = await sendVerifyBillSMS(
-                // {mobile: destinationNumber, contactName, billLink, orderNumber}
-            {mobile:destinationNumber, contactName:the_user_name, orderName:orderName,orderPrice,DATE:billDate, orderNumber}
-            )
+            if (adminSettings.sendSMSAfterSubmitBill) {
+                return await sendSMS("submitBill");
+            }
         }
-
-        sendSmsResult.status
-    };
-
-    debugger
-    // Send SMS based on the bill status and settings
-    if (isBillVerified && adminSettings.sendSMSAfterSubmitBill) {
-        return await sendSMS("verifyBill");
-    } else if (adminSettings.sendSMSAfterVerifyBill) {
-        return await sendSMS("submitBill");
+        return false;
     }
-
-    return false;
-};
+;
