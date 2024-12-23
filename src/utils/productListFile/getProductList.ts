0dc704c346cf85f2f0a61d6daa7b28427dc6f189
mohammad mirarab <mrm.learn@gmail.com>
2024-12-23 10:50:00 +0330

    update

---
+++ src/utils/productListFile/getProductList.ts
@@ -0,0 +1,29 @@
+import {readProductList} from "./readProductList";
+
+let CacheFile:any [] | null = null;
+let lastCacheUpdate: number | null = null; // زمان آخرین به‌روزرسانی کش
+const CACHE_TTL = 1000 * 60 * (6 * 60); // 6*60  دقیقه
+
+// تابع برای مدیریت کش
+const getCacheData = async (): Promise<any[] | null> => {
+    const now = Date.now();
+
+    if (!CacheFile || !lastCacheUpdate || (now - lastCacheUpdate > CACHE_TTL)) {
+        CacheFile = await readProductList()
+        lastCacheUpdate = now; // به‌روزرسانی زمان کش
+    } else {
+        // console.log("Using cached data.");
+    }
+
+    return CacheFile;
+};
+
+export const getProductList = async ({filters}: { filters: any[] }) => {
+
+    const allData = await getCacheData();
+
+    // شاید بخوایم محصولاتی که قیمت فروششون صفر هست رو نشون ندیم.
+   // اگه محاسبات یا فیلتری بخوایم روی فایل انجام بدیم میتونیم اینجا انجام بدیم. ولی دقت کنیم که توی تمام جاهایی که استفاده شده فیلتر اعمال میشه
+
+    return allData;
+};
