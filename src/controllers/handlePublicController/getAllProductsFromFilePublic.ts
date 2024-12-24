928bb49595400b7a077975c3970cf39697d25d0e
mohammad mirarab <mrm.learn@gmail.com>
2024-12-24 18:25:15 +0330

    update

--- src/controllers/handlePublicController/getAllProductsFromFilePublic.ts
+++ src/controllers/handlePublicController/getAllProductsFromFilePublic.ts
@@ -1,32 +1,17 @@
 import {NextFunction, Response} from 'express';
 import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
-import {IUser, User} from "../../models/User";
-import fs from 'fs';
-import path from 'path';
+import {getProductList} from "../../utils/productListFile/getProductList";
 
 const getAllProductsFromFilePublic = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
 
 
-
     try {
-        const filePath = path.join(__dirname, 'products.json');
-        // Check if the file exists
-        if (!fs.existsSync(filePath)) {
-            res.status(500).json({message: 'فایل محصولات یافت نشد'});
-            return;
-        }
-        // Read data from the file
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                res.status(500).json({message: 'خطا در خواندن فایل محصولات'});
-                return;
+
+        const products = await getProductList()
+        res.status(200).json({
+            data: {
+                List: products
             }
-            const products = JSON.parse(data);
-            res.status(200).json({
-                data: {
-                    List: products
-                }
-            });
         });
     } catch (error) {
         res.status(500).json({error});
