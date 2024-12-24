d52c192502915de8709b3d1f5b228fd53031e78b
mohammad mirarab <mrm.learn@gmail.com>
2024-12-24 16:56:37 +0330

    update

---
+++ src/controllers/hesabfaController/updateBillStatus.ts
@@ -0,0 +1,113 @@
+import {NextFunction, Response} from 'express';
+import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
+import {ACCESS_LIST} from "../../utils/ACCESS_LIST";
+import {checkAccessList} from "../../utils/checkAccessList";
+import {IUser, User} from "../../models/User";
+import {openTagDataChangeStatus, openTagDataForBasteBandi} from "../utility/collectionsHandlers/openTagData";
+import {sendSMSBasteBandi} from "../../SMS/SMS.IR/sendSms";
+import {hesabfaApiRequest} from "../utility/hesabfa/functions";
+import {logEvents} from "../../utils/logEvents";
+import {getCurrentTimeStamp} from "../../utils/timing";
+import generateRandomUUID from "../../utils/generateRandomUUID";
+
+const billStatusNumber = {
+    BASTED_BANDI: 5710,
+    TASFIE_SHODE: 5711,
+    AMADE_ERSAL: 5712,
+    ERSAL_SHODE: 5713,
+}
+
+const updateBillStatus = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
+
+    const {myToken} = req;
+    if (!myToken) {
+        res.status(200).json({message: 'مقدار توکن توی ری کوئست موجود نیست'});
+        return;
+    }
+
+    try {
+        // اینجا باید دسترسی رو چک کنم
+        // const hasAccessTo = await checkAccessList({
+        //     phoneNumber: myToken.phoneNumber,
+        //     arrayListToCheck: [ACCESS_LIST.basteBandi]
+        // });
+        //
+        // if (!hasAccessTo) {
+        //     res.status(403).json({message: 'شما مجوز دسترسی به این بخش را ندارید.'});
+        //     return;
+        // }
+
+        const userId = myToken?.UserInfo?.userData?.userData?.userId;
+        const foundUser: IUser | null = await User.findOne({_id: userId}).lean();
+
+        if (!foundUser) {
+            res.status(401).json({message: 'کاربری با این شماره تلفن یافت نشد'});
+            return;
+        }
+
+        const {invoice, statusNumber, description} = req.body;
+
+        if (!invoice?.ContactCode) {
+            res.status(500).json({message: 'کد مشتری برای این فاکتور تعریف نشده است.'});
+            return;
+        }
+
+        if (!invoice || !statusNumber) {
+            res.status(500).json({message: 'مقدارهای مورد نیاز ورودی در بدنه درخواست وجود ندارد.'});
+            return;
+        }
+
+        const url = 'invoice/save';
+        const lastTag =  invoice.Tag
+        const newTag = openTagDataChangeStatus({lastTag, statusNumber, description});
+        const newInvoice = {...invoice, Tag: newTag};
+
+        const result = await hesabfaApiRequest(url, {invoice: newInvoice})
+
+        if (result.response?.data.Result) {
+            const currentTimeStamp = getCurrentTimeStamp();
+            const dateString = new Date(currentTimeStamp).toLocaleString('fa-ir');
+            const userFullName = `${foundUser.name} ${foundUser.familyName}`;
+            const logMessage = `${generateRandomUUID()}\t${currentTimeStamp}\t${dateString}\t${foundUser.id}\t${userFullName} changed Tag From ${lastTag} to ${newTag}.`;
+            await logEvents(logMessage, "changeStatusBillEvents.txt");
+            let message = " وضعیت فاکتور تغییر کرد"
+
+            const mobile = result.response.data.Result.Contact.Mobile;
+            const ContactTitle = result.response.data.Result.ContactTitle;
+
+            if (mobile && ContactTitle) {
+
+                if (statusNumber === billStatusNumber.BASTED_BANDI) {
+                    const smsResult = await sendSMSBasteBandi({mobile, ORDERNAME: ContactTitle});
+                    if (smsResult.status) {
+                        message += " پیامک ارسال شد";
+                    } else {
+                        message += " پیامک ارسال نشد.";
+                    }
+                }
+                if (statusNumber === billStatusNumber.ERSAL_SHODE) {
+                    // const smsResult = await sendSMSBasteBandi({mobile, ORDERNAME: ContactTitle});
+                    // if (smsResult.status) {
+                    //     message += " پیامک ارسال شد";
+                    // } else {
+                    //     message += " پیامک ارسال نشد.";
+                    // }
+                }
+            }
+
+
+            res.status(200).json({message, res: result.response.data.Result});
+            return;
+        } else {
+            res.status(500).json({message: result?.response?.data?.ErrorMessage});
+            return;
+        }
+
+    } catch (error: any) {
+        const statusCode = error?.status || 500;
+        res.status(statusCode).json({message: error?.toString()});
+        return;
+    }
+};
+
+export {updateBillStatus};
