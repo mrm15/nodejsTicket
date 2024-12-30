a610941c41e619eb726b4ca0e3211c1352713a74
mohammad mirarab <mrm.learn@gmail.com>
2024-12-30 04:05:34 +0330

    update

--- src/websocket/websocketServer.ts
+++ src/websocket/websocketServer.ts
@@ -1,6 +1,9 @@
 // src/websocket/websocketServer.ts
-import { Server } from 'socket.io';
-import { Server as HttpServer } from 'http';
+import {Server} from 'socket.io';
+import {Server as HttpServer} from 'http';
+import {handleConnection} from "./handleConnection/handleConnection";
+import {identifyHandler} from "./identifyHandler/identifyHandler";
+import {handleDisconnect} from "./handleDisconnect/handleDisconnect";
 
 export const initializeSocketIO = (httpServer: HttpServer) => {
     // Initialize Socket.IO
@@ -12,19 +15,25 @@ export const initializeSocketIO = (httpServer: HttpServer) => {
     });
 
     // Handle WebSocket connections
-    io.on('connection', (socket) => {
+    io.on('connection', async (socket) => {
         console.log('A user connected');
+
+        // Listen for the 'identify' event
+        // Listen for the 'identify' event
+        socket.on('identify', (data) => {
+            identifyHandler({socket, data}); // Call the separate handler
+        });
 
         // Example event: sending a message to the server
         socket.on('message', (data) => {
             console.log('Received message:', data);
             // You can emit a response if needed
-            socket.emit('response', { message: 'Message received!' });
+            socket.emit('response', {message: 'Message received!'});
         });
 
         // Handle disconnection
         socket.on('disconnect', () => {
-            console.log('A user disconnected');
+            handleDisconnect({socket, data:socket.data})
         });
     });
 
