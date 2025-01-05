c354d8a50408e41e651c9d9d501ee0b9c85cd593
mohammad mirarab <mrm.learn@gmail.com>
2025-01-05 22:54:33 +0330

    update

--- src/utils/forwardTicketUtils/forwardTicket.ts
+++ src/utils/forwardTicketUtils/forwardTicket.ts
@@ -1,5 +1,6 @@
 import changeTicketsDepartmentAndUser from "./changeTicketsDepartmentAndUser";
 import addToAssignedTickets from "./addToAssignedTickets";
+import mongoose from "mongoose";
 
 interface IForwardTicket {
     ticketIdsArray: any[],
@@ -16,8 +17,10 @@ const forwardTicket = async ({ticketIdsArray, departmentId, userId, senderUserId
         departmentId,
         userId
     })
+    const newDepartmentId = departmentId ? new  mongoose.Types.ObjectId(departmentId) : null
+    const newUserId =  userId ? new mongoose.Types.ObjectId(userId) : null
     // تا اینجا یه سری از تیکت ها یا شایدم همه تغییر داده شدن و مقصد و کاربرشون تغییر کرد
-    const resultOfAddToAssignedTickets = await addToAssignedTickets({ticketIdsArray: ticketIdsArray, departmentId, userId, senderUserId})
+    const resultOfAddToAssignedTickets = await addToAssignedTickets({ticketIdsArray: ticketIdsArray, departmentId:newDepartmentId, userId:newUserId, senderUserId})
     //  و تا اینجا هم تیکت ایجاد شده و توی لیست تیکت ها رفته حالا اگه قراره نوتیف بدم باید اینجا بدم
 
     return {
