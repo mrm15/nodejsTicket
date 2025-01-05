876ea5e1c42a619f72f9fb4df61d383ba0c2146b
mohammad mirarab <mrm.learn@gmail.com>
2025-01-06 00:50:36 +0330

    update

--- src/utils/ticketAssigmentUtils/readDepartmentTicketsControllerUtils/processFilterOperators.ts
+++ src/utils/ticketAssigmentUtils/readDepartmentTicketsControllerUtils/processFilterOperators.ts
@@ -2,54 +2,68 @@ const processFilterOperators = (filter: any) => {
     const filterObject: any = {};
     const filterField = filter.property;
 
+    const isDate = (value: any): boolean => {
+        return !isNaN(Date.parse(value)); // Check if the value can be parsed into a valid date
+    };
+
     if (filter.operator) {
         if (!filterObject[filterField]) {
             filterObject[filterField] = {};
         }
+
+        // Convert filter value to a Date if applicable
+        const filterValue = isDate(filter.value) ? new Date(filter.value) : filter.value;
+
         switch (filter.operator) {
             case '>=':
-                filterObject[filterField]['$gte'] = filter.value;
+                filterObject[filterField]['$gte'] = filterValue;
                 break;
             case '>':
-                filterObject[filterField]['$gt'] = filter.value;
+                filterObject[filterField]['$gt'] = filterValue;
                 break;
             case '<=':
-                filterObject[filterField]['$lte'] = filter.value;
+                filterObject[filterField]['$lte'] = filterValue;
                 break;
             case '<':
-                filterObject[filterField]['$lt'] = filter.value;
+                filterObject[filterField]['$lt'] = filterValue;
                 break;
             case '!=':
-                filterObject[filterField]['$ne'] = filter.value;
+                filterObject[filterField]['$ne'] = filterValue;
                 break;
             case '===':
             case '==':
             case '=':
-                filterObject[filterField] = filter.value; // Exact match
+                filterObject[filterField] = filterValue; // Exact match
                 break;
             case 'in':
-                filterObject[filterField]['$in'] = filter.value;
+                filterObject[filterField]['$in'] = Array.isArray(filterValue)
+                    ? filterValue.map(value => (isDate(value) ? new Date(value) : value))
+                    : filterValue;
                 break;
             case 'nin':
-                filterObject[filterField]['$nin'] = filter.value;
+                filterObject[filterField]['$nin'] = Array.isArray(filterValue)
+                    ? filterValue.map(value => (isDate(value) ? new Date(value) : value))
+                    : filterValue;
                 break;
             case 'exists':
-                filterObject[filterField]['$exists'] = filter.value;
+                filterObject[filterField]['$exists'] = filterValue;
                 break;
             case 'type':
-                filterObject[filterField]['$type'] = filter.value;
+                filterObject[filterField]['$type'] = filterValue;
                 break;
             case 'regex':
             case 'includes':
             case '*':
-                filterObject[filterField] = { $regex: filter.value, $options: 'i' }; // Case-insensitive
+                filterObject[filterField] = { $regex: filterValue, $options: 'i' }; // Case-insensitive
                 break;
             default:
                 throw new Error(`Unsupported operator: ${filter.operator}`);
         }
     } else {
-        filterObject[filterField] = filter.value; // Default equality if no operator is provided
+        filterObject[filterField] = isDate(filter.value) ? new Date(filter.value) : filter.value; // Default equality
     }
+
     return filterObject;
-}
-export default processFilterOperators
+};
+
+export default processFilterOperators;
