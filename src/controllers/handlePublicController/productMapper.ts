9e4fa7e99c1a1ea0d9149506a74d50b065d12d70
mohammad mirarab <mrm.learn@gmail.com>
2025-01-12 00:56:49 +0330

    update

--- src/controllers/handlePublicController/productMapper.ts
+++ src/controllers/handlePublicController/productMapper.ts
@@ -4,13 +4,11 @@ export const productMapper = (productList: any[]) => {
     // اینجا اون آرایه اصلی رو میاریم و روی آیتم ها ساب منو  حلقه میزنیم تا دیتا رو در بیاریم
     const myMainArray = [...mainArray]
     myMainArray.forEach(mainRow => {
-        // mainRow
         mainRow.subMenu.forEach(subMenuRow => {
-            subMenuRow.items.forEach(itemsRow => {
-                const foundProduct = productList.find(singleItem => singleItem.Code === itemsRow.Code)
-                itemsRow = {
-                    ...itemsRow,
-                    ...foundProduct
+            subMenuRow.items.forEach(itemRow => {
+                const foundProduct = productList.find(singleItem => singleItem.Code === itemRow.Code) || null;
+                if (foundProduct) {
+                    Object.assign(itemRow, foundProduct); // Updates `itemRow` properties directly
                 }
             })
         })
