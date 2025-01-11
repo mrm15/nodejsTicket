e87a924e14610857821ede76a678a827b1c96694
mohammad mirarab <mrm.learn@gmail.com>
2025-01-12 00:56:45 +0330

    update

--- src/controllers/handlePublicController/getAllProductsFromFilePublic.ts
+++ src/controllers/handlePublicController/getAllProductsFromFilePublic.ts
@@ -1,16 +1,21 @@
 import {NextFunction, Response} from 'express';
 import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
 import {getProductList} from "../../utils/productListFile/getProductList";
-
+import {productMapper} from "./productMapper";
 const getAllProductsFromFilePublic = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
 
 
     try {
 
-        const products = await getProductList()
+        // این تابع خودش هر 6 ساعت یه بار به روز میشه
+        const allProducts = await getProductList();
+        debugger
+        const neededProducts = productMapper(allProducts || [])
+        // این تایه هم در صورتی که 6 ساعت رد شده باشه باید دوباره آجکت رو بسازه در غیر این صورت دیتا قبلی رو بده
+        // const
         res.status(200).json({
             data: {
-                List: products
+                List: neededProducts
             }
         });
     } catch (error) {
