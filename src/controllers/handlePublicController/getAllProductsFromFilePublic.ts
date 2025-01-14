aa7ea81df0da8069ccee59cbb29fe80016e3dbbd
mohammad mirarab <mrm.learn@gmail.com>
2025-01-15 00:51:07 +0330

    update

--- src/controllers/handlePublicController/getAllProductsFromFilePublic.ts
+++ src/controllers/handlePublicController/getAllProductsFromFilePublic.ts
@@ -2,6 +2,7 @@ import {NextFunction, Response} from 'express';
 import {CustomRequestMyTokenInJwt} from "../../middleware/verifyJWT";
 import {getProductList} from "../../utils/productListFile/getProductList";
 import {productMapper} from "./productMapper";
+import {addLog} from "../../utils/logMethods/addLog";
 const getAllProductsFromFilePublic = async (req: CustomRequestMyTokenInJwt, res: Response, next: NextFunction) => {
 
 
@@ -13,13 +14,25 @@ const getAllProductsFromFilePublic = async (req: CustomRequestMyTokenInJwt, res:
         const neededProducts = productMapper(allProducts || [])
         // این تایه هم در صورتی که 6 ساعت رد شده باشه باید دوباره آجکت رو بسازه در غیر این صورت دیتا قبلی رو بده
         // const
+        await addLog({
+            req: req,
+            phoneNumber: req?.myToken?.phoneNumber || "00000000000",
+            description: "صفحه ی قیمت های محصولات مشاهده شد!",
+            statusCode: 200,
+        })
         res.status(200).json({
             data: {
                 List: neededProducts,
             }
         });
-    } catch (error) {
-        res.status(500).json({error});
+    } catch (error:any) {
+        await addLog({
+            req: req,
+            phoneNumber: req?.myToken?.phoneNumber || "000000",
+            description: "خطا در لود صفحه ی قیمت ها!",
+            statusCode: 500,
+        })
+        res.status(500).json({error: error.toString()});
         return;
     }
 };
