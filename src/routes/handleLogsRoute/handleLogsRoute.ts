35c35a8a1f296a10be44dbad322be83d7a99a59f
mohammad mirarab <mrm.learn@gmail.com>
2025-01-17 15:57:29 +0330

    update

--- src/routes/handleLogsRoute/handleLogsRoute.ts
+++ src/routes/handleLogsRoute/handleLogsRoute.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import {readLogsController} from "../../controllers/logsController/readLogsController";
+import {deleteLogController} from "../../controllers/logsController/deleteLogController";
 
 
 const router = express.Router();
 
 router.post('/read', readLogsController);
-// router.delete('/delete/:id', deleteUserController);
+router.delete('/delete/', deleteLogController);
 // router.get('/info', getUserInfo);
 // router.post('/updateInfo', updateHisInfo); // update by id in token  user is changing his info
 
