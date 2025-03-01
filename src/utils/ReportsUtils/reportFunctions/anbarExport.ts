0b3ef211a4cef6e82269f1a5410cd1ef4ec43536
mohammad mirarab <mrm.learn@gmail.com>
2025-03-01 12:47:20 +0330

    update

--- src/utils/ReportsUtils/reportFunctions/anbarExport.ts
+++ src/utils/ReportsUtils/reportFunctions/anbarExport.ts
@@ -3,25 +3,20 @@ import { calculateSingleObject } from "./calculatePivotById";
 
 export const anbarExport = (totalData: any) => {
     const filters = [
-        { id: "000139", caption: "SMD سفید با سیم کشی و منگنه" },
-        { id: "000141", caption: "SMD انبه ای با سیم کشی و منگنه" },
-        { id: "000142", caption: "SMD قرمز با سیم کشی و منگنه" },
-        { id: "000140", caption: "SMD آفتابی با سیم کشی و منگنه" },
-        { id: "000143", caption: "اینجکشن آفتابی با سیم کشی و منگنه" },
-        { id: "000051", caption: "اینجکشن سفید با سیم کشی و منگنه" },
-        { id: "000369", caption: "اینجکشن انبه ای با سیم کشی و منگنه" },
-        { id: "000585", caption: "اینجکشن قرمز با سیم کشی و منگنه" },
-        { id: "000050", caption: "SMD انبه ای" },
-        { id: "000401", caption: "اینجکشن قرمز" },
-        { id: "000581", caption: "SMD اینجکشن انبه ای" },
-        { id: "000053", caption: "SMD اینجکشن آفتابی" },
-        { id: "000052", caption: "SMD اینجکشن سفید" },
-        { id: "000048", caption: "SMD آفتابی" },
-        { id: "000058", caption: "SMD سفید تک نقطه" },
+        { id: ["000047", "000139"], caption: "SMD سفید" },
+        { id: ["000051", "000052"], caption: "اینجکشن سفید با سیم کشی و منگنه" },
+        { id: ["000842"], caption: "SMD اینجکشن سفید 220 ولت" },
+        { id: ["000612", "000611"], caption: "SMD اینجکشن لنزدار سفید(سامسونگ)" },
+        { id: ["000048", "000140"], caption: "SMD آفتابی" },
+        { id: ["000053", "000143"], caption: "SMD اینجکشن آفتابی" },
+        { id: ["000049", "000142"], caption: "SMD قرمز" },
+        { id: ["000401", "000585"], caption: "اینجکشن قرمز" },
+        { id: ["000050", "000141"], caption: "SMD انبه ای" },
+        { id: ["000581", "000369"], caption: "SMD اینجکشن انبه ای" },
     ];
 
     const results = filters.map(({ id, caption }) =>
-        calculateSingleObject(totalData, { id: "", caption, ...keys, filterIdForPivot: [id] })
+        calculateSingleObject(totalData, { id: "", caption, ...keys, filterIdForPivot: [...id] })
     );
 
     return results;
