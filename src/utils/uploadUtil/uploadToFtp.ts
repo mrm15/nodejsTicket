cd44f32e408be6a35a44d55c54c46d4782f9fe26
mohammad mirarab <mrm.learn@gmail.com>
2025-03-06 01:09:28 +0330

    update

---
+++ src/utils/uploadUtil/uploadToFtp.ts
@@ -0,0 +1,63 @@
+// ftpUploader.ts
+import {Client} from "basic-ftp";
+import {Readable} from "stream";
+import {ftpPool} from "./ftpPool";
+
+/**
+ * Converts a Buffer to a Readable stream.
+ * @param buffer The Buffer to convert.
+ * @returns A Readable stream.
+ */
+function bufferToStream(buffer: Buffer): Readable {
+    return Readable.from([buffer]);
+}
+
+/**
+ * Uploads a file to the FTP server using environment variables for configuration.
+ * @param fileBuffer The file data as a Buffer.
+ * @param remoteFileName The remote filename including the desired path.
+ */
+export async function uploadToFtp(fileBuffer: Buffer, remoteFileName: string): Promise<void> {
+
+    const client = await ftpPool.getClient();
+    try {
+        const stream = bufferToStream(fileBuffer);
+        await client.uploadFrom(stream, `/data/uploads/${remoteFileName}`);
+    } catch (error) {
+        console.error("FTP upload error:", error);
+        throw error;
+    } finally {
+        ftpPool.releaseClient(client);
+    }
+
+
+    // const client = new Client();
+    // client.ftp.verbose = true;
+    //
+    // // Read FTP connection details from environment variables
+    // const host = process.env.FTP_HOST;
+    // const user = process.env.FTP_USER;
+    // const password = process.env.FTP_PASSWORD;
+    // const secure = process.env.FTP_SECURE === "true";
+    //
+    // if (!host || !user || !password) {
+    //     throw new Error("FTP configuration is missing in environment variables.");
+    // }
+    //
+    // try {
+    //     await client.access({
+    //         host,
+    //         user,
+    //         password,
+    //         secure,
+    //     });
+    //
+    //     const stream = bufferToStream(fileBuffer);
+    //     await client.uploadFrom(stream, `/data/uploads/${remoteFileName}`);
+    // } catch (error) {
+    //     console.error("FTP upload error:", error);
+    //     throw error;
+    // } finally {
+    //     client.close();
+    // }
+}
