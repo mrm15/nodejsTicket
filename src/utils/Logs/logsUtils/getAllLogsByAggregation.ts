98f1dbaa29bff083eb13e7887b2f08ddafcf1f07
mohammad mirarab <mrm.learn@gmail.com>
2025-03-15 13:50:45 +0330

    update

---
+++ src/utils/Logs/logsUtils/getAllLogsByAggregation.ts
@@ -0,0 +1,50 @@
+import processFilterOperators
+    from "../../ticketAssigmentUtils/readDepartmentTicketsControllerUtils/processFilterOperators";
+import {createAggregationPipelineForLogs} from "../createAggregationPipelineForLogs";
+import {Ticket} from "../../../models/ticket";
+import {LogModel} from "../../../models/logs";
+
+interface inputObjectGetDataByAggregation2 {
+    filters: any[],
+    page: number,
+    pageSize: number
+}
+
+const getAllLogsByAggregation = async ({filters, page, pageSize}: inputObjectGetDataByAggregation2) => {
+
+    const matchConditions:any =[]
+    // Apply filters dynamically (if any exist in the payload)
+    if (filters && filters.length > 0) {
+        filters.forEach((filter: any) => {
+            matchConditions.push(processFilterOperators(filter));
+        });
+    }
+
+    // console.log(matchConditions)
+    // Create the pipeline using match conditions and pagination details
+    const myPipelineForTickets = createAggregationPipelineForLogs({matchConditions, page, pageSize});
+
+    const result = await LogModel.aggregate(myPipelineForTickets);
+
+
+    // Extract results and total document count
+    const results = result[0]?.results;
+    const totalDocuments = result[0]?.totalDocuments?.length > 0 ? result[0]?.totalDocuments[0]?.total : 0;
+
+
+    const startIndex =( page - 1) * pageSize;
+    results.forEach((row: any, index: number) => {
+        row.rowNumber = startIndex + index + 1;
+    })
+
+    return {
+        results,           // Paginated results from aggregation
+        totalDocuments,    // Total count from facet pipeline
+        currentPage: page,       // The current page number
+        pageSize,        // The page size used for pagination
+
+        // allResultData: result
+    }
+};
+
+export default getAllLogsByAggregation;
